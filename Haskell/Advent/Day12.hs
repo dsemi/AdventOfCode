48696355f1fd6732864c49141838b36260f30463
Dan Seminara <semi225599@gmail.com>
2015-12-12 17:36:04 -0500

    Improve Day 12

--- Haskell/Advent/Day12.hs
+++ Haskell/Advent/Day12.hs
@@ -9,23 +9,24 @@ import Data.Aeson
 import Data.ByteString.Lazy.Char8 (ByteString, pack)
 import Data.HashMap.Strict (elems)
 import Data.Maybe
-import Data.Scientific
+import Data.Text (Text)
 import qualified Data.Vector as V
 
-readJson :: ByteString -> Maybe Value
-readJson = decode
+readJson :: ByteString -> Value
+readJson = fromMaybe (Number 0) . decode
 
-getNumbers :: Maybe Value -> Value -> Scientific
-getNumbers noval (Object m) = let vals = elems m
-                              in if isJust noval && fromJust noval `elem` vals then 0
-                                 else sum . map (getNumbers noval) $ elems m
-getNumbers noval (Array a)  = V.sum $ V.map (getNumbers noval) a
-getNumbers noval (Number n) = n
-getNumbers noval (String s) = 0
-getNumbers noval (Bool n)   = 0
+sumNumbers :: Maybe Text -> Value -> Int
+sumNumbers Nothing (Object m) = sum . map (sumNumbers Nothing) $ elems m
+sumNumbers noval@(Just s) (Object m)
+    | String s `elem` m       = 0
+    | otherwise               = sum . map (sumNumbers noval) $ elems m
+sumNumbers noval (Array a)    = V.sum $ V.map (sumNumbers noval) a
+sumNumbers noval (Number n)   = truncate n
+sumNumbers noval (String s)   = 0
+sumNumbers noval (Bool n)     = 0
 
 part1 :: String -> String
-part1 = show . getNumbers Nothing . fromMaybe (Number 0) . readJson . pack
+part1 = show . sumNumbers Nothing . readJson . pack
 
 part2 :: String -> String
-part2 = show . getNumbers (Just $ String "red") . fromMaybe (Number 0) . readJson . pack
+part2 = show . sumNumbers (Just "red") . readJson . pack
