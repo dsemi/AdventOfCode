272b3187511a70048f775af45506ead5d75b72d0
Dan Seminara <semi225599@gmail.com>
2015-12-16 01:09:39 -0500

    refactor a bit

--- Haskell/Advent/Day16.hs
+++ Haskell/Advent/Day16.hs
@@ -9,17 +9,17 @@ import Data.HashMap.Strict (HashMap, (!))
 import qualified Data.HashMap.Strict as M
 import Text.Regex.PCRE.Heavy (re, scan)
 
-tape :: HashMap String Int
-tape = M.fromList [ ("children", 3)
-                  , ("cats", 7)
-                  , ("samoyeds", 2)
-                  , ("pomeranians", 3)
-                  , ("akitas", 0)
-                  , ("vizslas", 0)
-                  , ("goldfish", 5)
-                  , ("trees", 3)
-                  , ("cars", 2)
-                  , ("perfumes", 1)
+tape :: HashMap String (Int -> Bool)
+tape = M.fromList [ ("children", (==3))
+                  , ("cats", (==7))
+                  , ("samoyeds", (==2))
+                  , ("pomeranians", (==3))
+                  , ("akitas", (==0))
+                  , ("vizslas", (==0))
+                  , ("goldfish", (==5))
+                  , ("trees", (==3))
+                  , ("cars", (==2))
+                  , ("perfumes", (==1))
                   ]
 
 parseLines :: String -> [HashMap String Int]
@@ -31,18 +31,19 @@ parseLines s = [ M.fromList [(k1, a1'), (k2, a2'), (k3, a3')]
                ]
     where regex = [re|Sue \d+: ([a-z]+): (\d+), ([a-z]+): (\d+), ([a-z]+): (\d+)|]
 
-couldMatch :: HashMap String Int -> Bool
-couldMatch = all (\(k, v) -> tape ! k == v) . M.toList
+couldMatch :: HashMap String (Int -> Bool) -> HashMap String Int -> Bool
+couldMatch tape = all (uncurry (tape !)) . M.toList
 
-couldMatch2 :: HashMap String Int -> Bool
-couldMatch2 = all match . M.toList
-    where match (k, v)
-              | k `elem` ["cats", "trees"]           = tape ! k < v
-              | k `elem` ["pomeranians", "goldfish"] = tape ! k > v
-              | otherwise                            = tape ! k == v
+solve :: HashMap String (Int -> Bool) -> String -> String
+solve tape' = show . fst . head . filter (couldMatch tape' . snd) . zip [1..] . parseLines
 
 part1 :: String -> String
-part1 = show . fst . head . filter (couldMatch . snd) . zip [1..] . parseLines
+part1 = solve tape
 
 part2 :: String -> String
-part2 = show . fst . head . filter (couldMatch2 . snd) . zip [1..] . parseLines
+part2 = solve tape'
+    where tape' = M.fromList [ ("cats", (>7))
+                             , ("pomeranians", (<3))
+                             , ("goldfish", (<5))
+                             , ("trees", (>3))
+                             ] `M.union` tape
