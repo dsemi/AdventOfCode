19c637ab7f86e502f6206db1a2ed3777a0c3f58e
Dan Seminara <semi225599@gmail.com>
2015-12-25 01:47:35 -0500

    Day 25

--- Haskell/Advent/Day25.hs
+++ Haskell/Advent/Day25.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes, BangPatterns #-}
+
 module Advent.Day25
     ( part1
     , part2
@@ -5,8 +7,21 @@ module Advent.Day25
 
 import Advent.Problem
 
+import Text.Regex.PCRE.Heavy
+
+parseCoord input = let [r, c] = map (read . fst) $ scan regex input
+                   in (r, c)
+    where regex = [re|\d+|]
+
+makeCode = mc 20151125
+    where mc !c 0 = c
+          mc !c !n = mc (c * 252533 `rem` 33554393) $ n - 1
+
 part1 :: Problem
-part1 = Pure $ const 0
+part1 = Pure f
+    where f input = makeCode index
+              where (r, c) = parseCoord input
+                    index = sum [1..r+c-2] + c - 1
 
 part2 :: Problem
-part2 = Pure $ const 0
+part2 = PureS $ const ""
