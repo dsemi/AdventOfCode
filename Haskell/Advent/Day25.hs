1427caebf8c2e523d273563bac7db0fd997cc23a
Dan Seminara <semi225599@gmail.com>
2015-12-27 14:52:22 -0500

    faster day 25

--- Haskell/Advent/Day25.hs
+++ Haskell/Advent/Day25.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE QuasiQuotes, BangPatterns #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Advent.Day25
     ( part1
@@ -7,21 +7,22 @@ module Advent.Day25
 
 import Advent.Problem
 
+import Math.NumberTheory.Powers
 import Text.Regex.PCRE.Heavy
 
 parseCoord input = let [r, c] = map (read . fst) $ scan regex input
                    in (r, c)
     where regex = [re|\d+|]
 
-makeCode = mc 20151125
-    where mc !c 0 = c
-          mc !c !n = mc (c * 252533 `rem` 33554393) $ n - 1
+makeCode :: Integer -> Int
+makeCode n = fromInteger $ powerMod 252533 n 33554393 * 20151125 `mod` 33554393
 
 part1 :: Problem
 part1 = Pure f
     where f input = makeCode index
               where (r, c) = parseCoord input
-                    index = sum [1..r+c-2] + c - 1
+                    n = r + c - 1
+                    index = n * (n - 1) `div` 2 + c - 1
 
 part2 :: Problem
 part2 = PureS $ const ""
