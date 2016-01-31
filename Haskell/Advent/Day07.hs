11bdbf699790bb994868d81ad70d460230258c57
Dan Seminara <semi225599@gmail.com>
2016-01-31 16:21:46 -0500

    No need for parsec

--- Haskell/Advent/Day07.hs
+++ Haskell/Advent/Day07.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Advent.Day07
     ( part1
@@ -13,10 +13,8 @@ import Data.Function.Memoize
 import Data.HashMap.Strict (HashMap, (!))
 import qualified Data.HashMap.Strict as M
 import Data.List (foldl')
-import Data.Maybe
 import Data.Either.Utils
 import Data.String.Utils
-import Text.Regex.PCRE.Heavy (re, scan)
 
 type Id = Either String Int
 data Node = Const Id
@@ -26,13 +24,15 @@ data Node = Const Id
           | LShift Id Id
           | RShift Id Id
 
-parseNode :: String -> Id -> Id -> Node
-parseNode ""       _ b = Const b
-parseNode "NOT"    _ b = Not b
-parseNode "AND"    a b = And a b
-parseNode "OR"     a b = Or a b
-parseNode "LSHIFT" a b = LShift a b
-parseNode "RSHIFT" a b = RShift a b
+parseNode :: String -> (String, Node)
+parseNode line = case words line of
+                   [             a, "->", v] -> (v, Const $ f a)
+                   [   "NOT",    a, "->", v] -> (v, Not $ f a)
+                   [a, "AND",    b, "->", v] -> (v, f a `And` f b)
+                   [a, "OR",     b, "->", v] -> (v, f a `Or` f b)
+                   [a, "LSHIFT", b, "->", v] -> (v, f a `LShift` f b)
+                   [a, "RSHIFT", b, "->", v] -> (v, f a `RShift` f b)
+    where f x = maybeToEither x $ maybeRead x
 
 eval :: (String -> Int) -> Node -> Int
 eval f exp = case exp of
@@ -46,12 +46,8 @@ eval f exp = case exp of
 
 buildWires :: [String] -> HashMap String Node
 buildWires = foldl' addWire M.empty
-    where regex = [re|(?:(?:(\S+) )?(\S+) )?(\S+) -> (\S+)|]
-          addWire :: HashMap String Node -> String -> HashMap String Node
-          addWire m s = let [a, op, b, w] = snd . head $ scan regex s
-                            a' = maybeToEither a $ maybeRead a
-                            b' = maybeToEither b $ maybeRead b
-                            node = parseNode op a' b'
+    where addWire :: HashMap String Node -> String -> HashMap String Node
+          addWire m s = let (w, node) = parseNode s
                         in M.insert w node m
 
 getValue :: HashMap String Node -> String -> Int
