b05697abe47f3ea2ef36967f29b2c5dd4c81c74c
Dan Seminara <semi225599@gmail.com>
2016-01-31 16:07:45 -0500

    c

--- Haskell/Advent/Day07.hs
+++ Haskell/Advent/Day07.hs
@@ -8,41 +8,70 @@ module Advent.Day07
 import Advent.Problem
 
 import Data.Bits
-import Data.HashMap.Lazy (HashMap, (!))
-import qualified Data.HashMap.Lazy as M
+import Data.Either
+import Data.Function.Memoize
+import Data.HashMap.Strict (HashMap, (!))
+import qualified Data.HashMap.Strict as M
 import Data.List (foldl')
 import Data.Maybe
+import Data.Either.Utils
 import Data.String.Utils
-import Debug.Trace
 import Text.Regex.PCRE.Heavy (re, scan)
 
-ops :: HashMap String (Int -> Int -> Int)
-ops = M.fromList [ ( "NOT", const complement )
-                 , ( "AND", (.&.) )
-                 , ( "OR", (.|.) )
-                 , ( "LSHIFT", shiftL )
-                 , ( "RSHIFT", shiftR )
-                 ]
-buildWires :: [String] -> HashMap String Int
-buildWires input = wires
-    where wires = foldl' addWire M.empty input
-          regex = [re|(?:(?:(\S+) )?(\S+) )?(\S+) -> (\S+)|]
-          addWire :: HashMap String Int -> String -> HashMap String Int
+type Id = Either String Int
+data Node = Const Id
+          | Not Id
+          | And Id Id
+          | Or Id Id
+          | LShift Id Id
+          | RShift Id Id
+
+parseNode :: String -> Id -> Id -> Node
+parseNode ""       _ b = Const b
+parseNode "NOT"    _ b = Not b
+parseNode "AND"    a b = And a b
+parseNode "OR"     a b = Or a b
+parseNode "LSHIFT" a b = LShift a b
+parseNode "RSHIFT" a b = RShift a b
+
+eval :: (String -> Int) -> Node -> Int
+eval f exp = case exp of
+               Const a    -> val a
+               Not a      -> complement $ val a
+               And a b    -> val a .&. val b
+               Or a b     -> val a .|. val b
+               LShift a b -> val a `shiftL` val b
+               RShift a b -> val a `shiftR` val b
+    where val = either f id
+
+
+buildWires :: [String] -> HashMap String Node
+buildWires input = foldl' addWire M.empty input
+    where regex = [re|(?:(?:(\S+) )?(\S+) )?(\S+) -> (\S+)|]
+          addWire :: HashMap String Node -> String -> HashMap String Node
           addWire m s = let [a, op, b, w] = snd . head $ scan regex s
-                            op' = fromMaybe (flip const) $ M.lookup op ops
-                            a'  = fromMaybe (if null a then 0 else wires ! a) $ maybeRead a
-                            b'  = fromMaybe (wires ! b) $ maybeRead b
-                        in M.insert w (op' a' b') m
+                            a' = maybeToEither a $ maybeRead a
+                            b' = maybeToEither b $ maybeRead b
+                            node = parseNode op a' b'
+                        in M.insert w node m
+
+getValue :: HashMap String Node -> String -> Int
+getValue m k = mgv k
+    where mgv = memoFix gv
+          gv f k = eval f $ m ! k
 
 p1 :: String -> Int
-p1 = (! "a") . buildWires . lines
+p1 input = let m = buildWires $ lines input
+           in getValue m "a"
 
 part1 :: Problem
 part1 = Pure p1
 
 p2 :: String -> Int
-p2 input = let wires = buildWires $ lines input ++ [show (p1 input) ++ " -> b"]
-           in wires ! "a"
+p2 input = let m = buildWires $ lines input
+               a = getValue m "a"
+               m' = M.insert "b" (Const $ Right a) m
+           in getValue m' "a"
 
 part2 :: Problem
 part2 = Pure p2
