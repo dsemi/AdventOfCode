8a3d706085ea52e2153cc6d2bae8be34e70ac657
Dan Seminara <semi225599@gmail.com>
2016-12-04 01:07:33 -0500

    Day 4

--- src/Year2016/Day04.hs
+++ src/Year2016/Day04.hs
@@ -3,8 +3,43 @@ module Year2016.Day04
     , part2
     ) where
 
+import Control.Arrow ((&&&))
+import Data.List (group, intercalate, isInfixOf, sort, sortBy)
+import Data.List.Split (splitOn)
+import Data.Maybe (fromJust)
+import Text.Megaparsec (char, noneOf, parseMaybe, some)
+import Text.Megaparsec.String (Parser)
+
+
+data Room = Room { encryptedName :: String
+                 , sectorId :: Int
+                 , checksum :: String
+                 }
+
+getRooms :: String -> [Room]
+getRooms = map (fromJust . parseMaybe parseRoom) . lines
+    where parseRoom :: Parser Room
+          parseRoom = do
+            encryptedPlusSector <- some (noneOf "[") <* char '['
+            checksum <- some (noneOf "]") <* char ']'
+            let ss = splitOn "-" encryptedPlusSector
+            return $ Room (intercalate "-" $ init ss) (read $ last ss) checksum
+
+roomIsValid :: Room -> Bool
+roomIsValid (Room en _ ch) = nCommonChars 5 (filter (/= '-') en) == ch
+    where nCommonChars n = take n . map snd . sort . map (negate . length &&& head) . group . sort
+
 part1 :: String -> String
-part1 = const ""
+part1 = show . sum . map sectorId . filter roomIsValid . getRooms
+
+rotate 0 c = c
+rotate n c
+    | c == '-' = ' '
+    | c == 'z' = rotate (n-1) 'a'
+    | otherwise= rotate (n-1) (succ c)
+
+isNorthPole :: Room -> Bool
+isNorthPole (Room en si _) = "northpole" `isInfixOf` map (rotate si) en
 
 part2 :: String -> String
-part2 = const ""
+part2 = show . sectorId . head . filter isNorthPole . filter roomIsValid . getRooms
