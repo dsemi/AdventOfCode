6fba9127b692e5eb5daee53ffee56699345052f3
Dan Seminara <semi225599@gmail.com>
2016-12-09 22:58:13 -0500

    Days 8 and 9

--- src/Year2016/Day08.hs
+++ src/Year2016/Day08.hs
@@ -1,10 +1,58 @@
+{-# LANGUAGE TupleSections #-}
+
 module Year2016.Day08
     ( part1
     , part2
     ) where
 
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as S
+import Data.List (foldl')
+import Data.Maybe (mapMaybe)
+import Text.Megaparsec ((<|>), parseMaybe, string)
+import Text.Megaparsec.Lexer (integer)
+import Text.Megaparsec.String (Parser)
+
+
+type Coord = (Int, Int)
+
+int = fromInteger <$> integer
+
+w = 50
+h = 6
+
+parseRect :: Parser (HashSet Coord -> HashSet Coord)
+parseRect = do
+  width <- string "rect " *> int
+  height <- string "x" *> int
+  return $ S.union $ S.fromList [(r, c) | c <- [0..width-1], r <- [0..height-1]]
+
+parseRotateRow :: Parser (HashSet Coord -> HashSet Coord)
+parseRotateRow = do
+  row <- string "rotate row y=" *> int
+  amt <- string " by " *> int
+  return $ S.map (\rc@(r, c) -> if r == row then (r, (c + amt) `mod` w) else rc)
+
+parseRotateCol :: Parser (HashSet Coord -> HashSet Coord)
+parseRotateCol = do
+  col <- string "rotate column x=" *> int
+  amt <- string " by " *> int
+  return $ S.map (\rc@(r, c) -> if c == col then ((r + amt) `mod` h, c) else rc)
+
+parser :: Parser (HashSet Coord -> HashSet Coord)
+parser = parseRect <|> parseRotateRow <|> parseRotateCol
+
+litPixels :: String -> HashSet Coord
+litPixels = foldl' (flip ($)) S.empty . mapMaybe (parseMaybe parser) . lines
+
 part1 :: String -> String
-part1 = const ""
+part1 = show . S.size . litPixels
+
+showDisplay :: HashSet Coord -> String
+showDisplay litPix = '\n' : (unlines $ map (\r -> map (f . (r,)) [0..w-1]) [0..h-1])
+    where f p
+              | S.member p litPix = '#'
+              | otherwise         = ' '
 
 part2 :: String -> String
-part2 = const ""
+part2 = showDisplay . litPixels
--- src/Year2016/Day09.hs
+++ src/Year2016/Day09.hs
@@ -3,8 +3,37 @@ module Year2016.Day09
     , part2
     ) where
 
+import Text.Megaparsec (char, parse)
+import Text.Megaparsec.Lexer (integer)
+import Text.Megaparsec.String (Parser)
+
+data Marker = Marker { dataLen :: Int
+                     , repeatCount :: Int
+                     , markerLen :: Int
+                     }
+
+parseMarker :: Parser Marker
+parseMarker = do
+  dl <- char '(' *> int
+  rc <- char 'x' *> int <* char ')'
+  return . Marker dl rc . length $ format dl rc
+    where int = fromInteger <$> integer
+          format a b = "(" ++ show a ++ "x" ++ show b ++ ")"
+
+decompressedLength :: (String -> Int) -> String -> Int
+decompressedLength _ "" = 0
+decompressedLength f input@(s:ss) =
+    case (parse parseMarker "" input) of
+      (Right marker) -> let input' = drop (markerLen marker) input
+                            (repeatedChars, rest) = splitAt (dataLen marker) input'
+                        in repeatCount marker * f repeatedChars + decompressedLength f rest
+      (Left _) -> 1 + decompressedLength f ss
+
 part1 :: String -> String
-part1 = const ""
+part1 = show . decompressedLength length
+
+recursive :: String -> Int
+recursive = decompressedLength recursive
 
 part2 :: String -> String
-part2 = const ""
+part2 = show . decompressedLength recursive
