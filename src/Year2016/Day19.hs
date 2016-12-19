d8caebadc540bd7d396277830422aa68913a0908
Dan Seminara <semi225599@gmail.com>
2016-12-19 18:11:27 -0500

    Better Day 19

--- src/Year2016/Day19.hs
+++ src/Year2016/Day19.hs
@@ -1,30 +1,30 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Year2016.Day19
     ( part1
     , part2
     ) where
 
-import Data.Sequence (Seq, ViewL(..))
+import Data.Sequence ((|>), Seq, ViewL(..))
 import qualified Data.Sequence as S
+
+removeNext :: [Int] -> [Int]
+removeNext = go []
+    where go c [ ] = reverse c
+          go c [x] = x : reverse c
+          go c (x:_:xs) = go (x:c) xs
+
+iterateUntil :: (a -> Bool) -> (a -> a) -> a -> a
+iterateUntil cond f x = head . filter cond $ iterate f x
+
+part1 :: String -> Int
+part1 s = head $ iterateUntil ((==1) . length) removeNext [1..(read s)]
+
+removeAcross :: Seq Int -> Seq Int
+removeAcross s@(S.viewl -> (x :< xs)) = S.deleteAt (length s `div` 2 - 1) xs |> x
 
 start :: Seq a -> a
 start = (\(l :< _) -> l) . S.viewl
 
-takePresent :: (Int -> Int -> Int) -> Int -> Seq Int -> Int
-takePresent f index s
-    | len > 1 = takePresent f nextIndex $ S.deleteAt indexToDelete s
-    | otherwise = start s
-    where len           = length s
-          indexToDelete = f index len
-          updatedIndex  = if indexToDelete < index
-                          then index - 1
-                          else index
-          nextIndex     = (updatedIndex + 1) `mod` (len - 1)
-
-sim :: (Int -> Int -> Int) -> Int -> Int
-sim f n = takePresent f 0 $ S.fromList [1..n]
-
-part1 :: String -> Int
-part1 = sim (\index len -> (index + 1) `mod` len) . read
-
 part2 :: String -> Int
-part2 = sim (\index len -> (index + len `div` 2) `mod` len) . read 
+part2 s = start . iterateUntil ((==1) . length) removeAcross $ S.fromList [1..(read s)]
