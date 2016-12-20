dc80d4e49cf363ebb8d58d42715d1cca4550628a
Dan Seminara <semi225599@gmail.com>
2016-12-20 01:33:10 -0500

    Day 20

--- src/Year2016/Day20.hs
+++ src/Year2016/Day20.hs
@@ -3,8 +3,25 @@ module Year2016.Day20
     , part2
     ) where
 
-part1 :: String -> String
-part1 = const ""
+import Data.List (sort)
+import Data.List.Split (splitOn)
 
-part2 :: String -> String
-part2 = const ""
+
+parseInput :: String -> [(Int, Int)]
+parseInput = map ((\[x,y] -> (read x, read y)) . splitOn "-") . lines
+
+ipFilter :: [(Int, Int)] -> [Int]
+ipFilter = go (-1) []
+    where upperBound = 4294967295
+          go x c []
+              | x < upperBound = c ++ [x..upperBound]
+              | otherwise      = c
+          go x c ((low, high) : rest)
+              | x+1 < low = go high (c++[x+1..low-1]) rest
+              | otherwise = go (max x high) c rest
+
+part1 :: String -> Int
+part1 = head . ipFilter . sort . parseInput
+
+part2 :: String -> Int
+part2 = length . ipFilter . sort . parseInput
