dd09971f9ab6e6a370296033640ff3f50e67a1b3
Dan Seminara <semi225599@gmail.com>
2016-12-23 02:10:02 -0500

    Day 23

--- src/Year2016/Day23h.hs
+++ src/Year2016/Day23h.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE TemplateHaskell, StrictData #-}
 
-module Year2016.Day12h where
+module Year2016.Day23h where
 
 import Data.Vector (Vector)
 import Control.Lens.TH (makeLenses)
 
-data Value = Reg Char | Const Int deriving (Show)
+data Value = Reg Char | Const Int deriving (Eq, Show)
 
 data Instruction = Cpy Value Char
                  | Inc Char
                  | Dec Char
-                 | Jnz Value Int deriving (Show)
+                 | Tgl Char
+                 | Jnz Value Value
+                 | Noop (Maybe Instruction)
+                 | Mul Char Char Char Char deriving (Eq, Show)
 
 data Simulator = Sim { _a :: Int
                      , _b :: Int
