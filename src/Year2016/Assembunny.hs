2daa013cd2a65d6c32819625e77ce78e657337c3
Dan Seminara <semi225599@gmail.com>
2017-09-21 20:06:54 -0400

    Check optimization at runtime in case tgl modifies code

--- src/Year2016/Assembunny.hs
+++ src/Year2016/Assembunny.hs
@@ -12,27 +12,24 @@ import Control.Lens ((%=), (.=), (+=), (-=), assign, ix, over, set, use)
 import Control.Lens.TH (makeLenses)
 import Control.Monad
 import Control.Monad.State.Strict
-import Data.Data
 import Data.Foldable()
 import Data.Maybe (fromJust, mapMaybe)
 import Data.Sequence (Seq, (|>), empty)
 import Data.Vector ((!), Vector)
 import qualified Data.Vector as V
-import Data.Vector.Lens (sliced)
 import Text.Megaparsec ((<|>), eitherP, oneOf, parseMaybe, space, spaceChar, string)
 import Text.Megaparsec.Lexer (integer, signed)
 import Text.Megaparsec.String (Parser)
 
 
-data Value = Reg Char | Const Int deriving (Eq, Show)
+data Value = Const Int | Reg Char deriving (Eq, Show)
 
 data Instruction = Cpy Value Value
                  | Inc Char
                  | Dec Char
                  | Tgl Char
                  | Out Char
-                 | Jnz Value Value
-                 | Mul Value Char Char Char deriving (Eq, Show)
+                 | Jnz Value Value deriving (Eq, Show)
 
 data Simulator = Sim { _a :: Int
                      , _b :: Int
@@ -76,75 +73,81 @@ value :: Value -> State Simulator Int
 value (Const i) = return i
 value (Reg   r) = use $ reg r
 
-evalInstr :: Instruction -> State Simulator ()
-evalInstr (Cpy v v') =
-    case v' of
-      (Const _) -> return ()
-      (Reg r)   -> value v >>= assign (reg r)
-evalInstr (Inc r) = reg r += 1
-evalInstr (Dec r) = reg r -= 1
-evalInstr (Tgl r) = do
-  i <- use $ reg r
-  instrs <- use instructions
-  cl <- use currentLine
-  when (i + cl < V.length instrs) $ do
-    modify' $ over (instructions . ix (i+cl)) tgl
-      where tgl (Cpy v v') = Jnz v v'
-            tgl (Inc r)    = Dec r
-            tgl (Dec r)    = Inc r
-            tgl (Jnz v v') = Cpy v v'
-            tgl (Tgl r)    = Inc r
-evalInstr (Out r) = do
-  v <- use $ reg r
-  output %= (|> v)
-evalInstr (Jnz v v') = do
-  l <- value v'
-  rv <- value v
-  when (rv /= 0) $
-       currentLine += l - 1
-evalInstr (Mul a b c d) = do
+multiplication :: [Instruction] -> Maybe (Value, Char, Char, Char)
+multiplication (Cpy a (Reg d)
+               : Inc c
+               : Dec d'
+               : Jnz (Reg d'') (Const (-2))
+               : Dec b
+               : Jnz (Reg b') (Const (-5))
+               : _)
+    | d == d' && d' == d'' && b == b' = Just (a, b, c, d)
+    | otherwise = Nothing
+multiplication _ = Nothing
+
+eval :: [Instruction] -> State Simulator (Int -> Int)
+eval (multiplication -> Just (a, b, c, d)) = do
   a' <- value a
   b' <- use $ reg b
   reg c += a' * b'
   reg b .= 0
   reg d .= 0
+  return (+6)
+eval (Cpy v v' : _) = do
+  case v' of
+    (Reg r)   -> value v >>= assign (reg r)
+    (Const _) -> return ()
+  return (+1)
+eval (Inc r : _) = do
+  reg r += 1
+  return (+1)
+eval (Dec r : _) = do
+  reg r -= 1
+  return (+1)
+eval (Tgl r : _) = do
+  i <- use $ reg r
+  instrs <- use instructions
+  cl <- use currentLine
+  when (i + cl < length instrs) $ do
+    modify' $ over (instructions . ix (i+cl)) tgl
+  return (+1)
+      where tgl (Cpy v v') = Jnz v v'
+            tgl (Inc r)    = Dec r
+            tgl (Dec r)    = Inc r
+            tgl (Jnz v v') = Cpy v v'
+            tgl (Tgl r)    = Inc r
+eval (Out r : _) = do
+  v <- use $ reg r
+  output %= (|> v)
+  return (+1)
+eval (Jnz v v' : _) = do
+  l <- value v'
+  rv <- value v
+  return $ if rv /= 0 then (+l) else (+1)
 
-optimize :: Vector Instruction -> Vector Instruction
-optimize v = foldr replaceMul v
-             $ mapMaybe (\i -> (i,) <$> matchesMul (V.slice i 6 v)) [0 .. V.length v - 6]
-    where matchesMul (V.toList -> [ Cpy a (Reg d)
-                                  , Inc c
-                                  , Dec d'
-                                  , Jnz (Reg d'') (Const (-2))
-                                  , Dec b
-                                  , Jnz (Reg b') (Const (-5))])
-              | d == d' && d' == d'' && b == b' = Just (a, b, c, d)
-              | otherwise = Nothing
-          matchesMul _ = Nothing
-          noop = Jnz (Const 0) (Const 0)
-          toMul a b c d = V.fromList $ Mul a b c d : replicate 5 noop
-          replaceMul (i,(a,b,c,d)) = set (sliced i 6) (toMul a b c d)
+evaluateUntil :: State Simulator Bool -> State Simulator ()
+evaluateUntil f = do
+  instrs <- use instructions
+  line <- use currentLine
+  cond <- f
+  when cond $ do
+    cl <- eval $ drop line $ V.toList instrs
+    currentLine %= cl
+    evaluateUntil f
 
-evaluateWith :: (Instruction -> State Simulator ()) -> Simulator -> Simulator
-evaluateWith evalInstr = execState eval . over instructions optimize
-    where eval :: State Simulator ()
-          eval = do
-            line <- use currentLine
-            instrs <- use instructions
-            when (line >= 0 && line < V.length instrs) $ do
-              evalInstr $ instrs ! line
-              currentLine += 1
-              eval
+lineInBounds :: State Simulator Bool
+lineInBounds = do
+  instrs <- use instructions
+  line <- use currentLine
+  return $ line >= 0 && line < V.length instrs
 
+outLengthIs :: Int -> State Simulator Bool
+outLengthIs n = do
+  out <- use output
+  return $ length out <= n
 
 evaluate :: Simulator -> Simulator
-evaluate = evaluateWith evalInstr
+evaluate = execState (evaluateUntil lineInBounds)
 
 evaluateUntilOutputLengthIs :: Int -> Simulator -> Simulator
-evaluateUntilOutputLengthIs n = evaluateWith evalInstr'
-    where evalInstr' (Out r) = do
-            evalInstr $ Out r
-            o <- use output
-            when (length o > n) $
-                 currentLine .= -100 -- Set to some value that will 'break' out of eval loop
-          evalInstr' x = evalInstr x
+evaluateUntilOutputLengthIs n = execState (evaluateUntil ((&&) <$> outLengthIs n <*> lineInBounds))
