6a9942922f471ef55245fe967961a46601af1dc6
Dan Seminara <semi225599@gmail.com>
2017-10-03 22:06:20 -0400

    Speed up Day 13

--- src/Year2015/Day13.hs
+++ src/Year2015/Day13.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Year2015.Day13
     ( part1
     , part2
@@ -7,14 +9,14 @@ import Utils
 
 import Data.HashMap.Strict (HashMap, (!))
 import qualified Data.HashMap.Strict as M
-import Data.List (permutations)
+import Data.List (permutations, foldl1')
 import Data.Maybe
 import Text.Megaparsec (parseMaybe, some, try, (<|>))
 import Text.Megaparsec.Char (alphaNumChar, char, spaceChar, string)
 import Text.Megaparsec.Char.Lexer (decimal)
 
 
-data Edge = Edge String String Int
+data Edge = Edge Char Char Int
 
 parseLine :: String -> Edge
 parseLine = fromJust . parseMaybe parser
@@ -33,20 +35,21 @@ parseLine = fromJust . parseMaybe parser
             string " happiness units by sitting next to "
             p2 <- some alphaNumChar
             char '.'
-            return $ Edge p1 p2 hap
+            return $ Edge (head p1) (head p2) hap
 
 
-
-constructMap :: [Edge] -> HashMap String (HashMap String Int)
+constructMap :: [Edge] -> HashMap Char (HashMap Char Int)
 constructMap = foldr addEdgeToMap M.empty
     where addEdgeToMap (Edge p1 p2 n) m = let m' = fromMaybe M.empty $ M.lookup p1 m
                                           in M.insert p1 (M.insert p2 n m') m
 
-maxHappinessOrdering :: HashMap String (HashMap String Int) -> Int
-maxHappinessOrdering m = maximum $ map (\p -> happinessDiff (head p) (last p)
-                                              + sum (zipWith happinessDiff p $ tail p)) orders
+maxHappinessOrdering :: HashMap Char (HashMap Char Int) -> Int
+maxHappinessOrdering m = maximum $ map happinessDiff orders
     where orders = permutations $ M.keys m
-          happinessDiff a b = m ! a ! b + m ! b ! a
+          happinessDiff xss@(x:_) = go 0 xss
+              where go !c [y] = c + hd x y
+                    go !c (a:b:xs) = go (c + hd a b) (b:xs)
+          hd a b = m ! a ! b + m ! b ! a
 
 part1 :: String -> Int
 part1 = maxHappinessOrdering . constructMap . map parseLine . lines
@@ -54,5 +57,5 @@ part1 = maxHappinessOrdering . constructMap . map parseLine . lines
 part2 :: String -> Int
 part2 input = let m = constructMap . map parseLine $ lines input
                   meMap = M.fromList . zip (M.keys m) $ repeat 0
-                  m' = M.insert "me" meMap $ M.map (M.insert "me" 0) m
+                  m' = M.insert 'X' meMap $ M.map (M.insert 'X' 0) m
               in maxHappinessOrdering m'
