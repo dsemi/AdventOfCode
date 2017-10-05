76a478b7d10e7d1257a30ed7287ebb0ce4d16506
Dan Seminara <semi225599@gmail.com>
2017-10-05 00:53:07 -0400

    Optimize 18 and 19

--- src/Year2016/Day18.hs
+++ src/Year2016/Day18.hs
@@ -3,16 +3,25 @@ module Year2016.Day18
     , part2
     ) where
 
-makeNextRow :: String -> String
-makeNextRow prevRow@(_:rest) = zipWith3 safeOrTrap ('.':prevRow) prevRow $ rest ++ "."
-    where safeOrTrap '^' '^' '.' = '^'
+import Data.Vector.Unboxed (Vector, (!))
+import qualified Data.Vector.Unboxed as V
+
+
+makeNextRow :: Vector Char -> Vector Char
+makeNextRow prevRow = V.imap fun prevRow
+    where vLastI = V.length prevRow - 1
+          fun i _ = safeOrTrap a b c
+              where a = if i == 0 then '.' else V.unsafeIndex prevRow (i-1)
+                    b = V.unsafeIndex prevRow i
+                    c = if i == vLastI then '.' else V.unsafeIndex prevRow (i+1)
+          safeOrTrap '^' '^' '.' = '^'
           safeOrTrap '.' '^' '^' = '^'
           safeOrTrap '^' '.' '.' = '^'
           safeOrTrap '.' '.' '^' = '^'
           safeOrTrap  _   _   _  = '.'
 
 numSafe :: Int -> String -> Int
-numSafe n = length . filter (=='.') . concat . take n . iterate makeNextRow
+numSafe n = sum . map (V.length . V.filter (== '.')) . take n . iterate makeNextRow . V.fromList
 
 part1 :: String -> Int
 part1 = numSafe 40
--- src/Year2016/Day19.hs
+++ src/Year2016/Day19.hs
@@ -5,23 +5,20 @@ module Year2016.Day19
     , part2
     ) where
 
-import Data.Sequence ((|>), Seq, ViewL(..))
-import qualified Data.Sequence as S
 
-removeNext :: [Int] -> [Int]
-removeNext = go []
-    where go c [ ] = reverse c
-          go c [x] = x : reverse c
-          go c (x:_:xs) = go (x:c) xs
+josephus :: Int -> Int
+josephus n = 1 + 2 * (n - 2 ^ largestPowerOf2)
+    where largestPowerOf2 = floor $ logBase 2 $ fromIntegral n
 
 part1 :: Int -> Int
-part1 n = head $ until ((==1) . length) removeNext [1..n]
+part1 = josephus
 
-removeAcross :: Seq Int -> Seq Int
-removeAcross s@(S.viewl -> (x :< xs)) = S.deleteAt (length s `div` 2 - 1) xs |> x
-
-start :: Seq a -> a
-start = (\(l :< _) -> l) . S.viewl
+weirdJosephus :: Int -> Int
+weirdJosephus n = let p3 = 3 ^ largestPowerOf3 n
+                      ans  = n - p3
+                      ans' = ans + max 0 (ans - p3)
+                  in if ans' == 0 then p3 else ans'
+    where largestPowerOf3 n = floor $ logBase 3 $ fromIntegral n
 
 part2 :: Int -> Int
-part2 n = start . until ((==1) . length) removeAcross $ S.fromList [1..n]
+part2 = weirdJosephus
