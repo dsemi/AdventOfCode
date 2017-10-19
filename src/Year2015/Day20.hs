27fbf90f6cae532201bf5c37cb520a1ac4865d82
Dan Seminara <semi225599@gmail.com>
2017-10-19 19:23:56 -0400

    Speed up Year 2015 Day 20 impl

--- src/Year2015/Day20.hs
+++ src/Year2015/Day20.hs
@@ -3,15 +3,34 @@ module Year2015.Day20
     , part2
     ) where
 
-import qualified Data.Set as S
-import Math.NumberTheory.ArithmeticFunctions
+import Control.Monad (forM_)
+import Control.Monad.ST (runST)
+import Data.Maybe (fromJust)
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as M
 
 
-part1 :: String -> Int
-part1 input = fromInteger . head $ dropWhile ((<minPresents) . (*10) . sigma 1) [1..]
-    where minPresents = read input
+part1 :: Int -> Int
+part1 n = fromJust $ V.findIndex (>=n) vec
+    where vec :: Vector Int
+          vec = runST $ do
+                  let lim = n `div` 10
+                  v <- M.new (lim + 1)
+                  M.set v 0
+                  forM_ [1..lim] $ \i -> do
+                    forM_ [i, 2*i..lim] $ \j -> do
+                      M.modify v (+ (i*10)) j
+                  V.unsafeFreeze v
 
-part2 :: String -> Int
-part2 input = fromInteger . head $ dropWhile (\n -> (<minPresents) . (*11) . S.foldl' (+) 0
-                                                    . S.filter ((>=n) . (*50)) $ divisors n) [1..]
-    where minPresents = read input
+part2 :: Int -> Int
+part2 n = fromJust $ V.findIndex (>=n) vec
+    where vec :: Vector Int
+          vec = runST $ do
+                  let lim = n `div` 11
+                  v <- M.new (lim + 1)
+                  M.set v 0
+                  forM_ [1..lim] $ \i -> do
+                    forM_ (take 50 [i, 2*i..lim]) $ \j -> do
+                      M.modify v (+ (i*11)) j
+                  V.unsafeFreeze v
