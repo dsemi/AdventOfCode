036fc45c473854a64710df661c8a50c8c15c02bd
Dan Seminara <semi225599@gmail.com>
2017-10-21 15:55:42 -0400

    Generalize multiprocess solution to Day 4 to be re-used in other situations where threading could speed up result

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Utils where
 
+import Control.Concurrent (setNumCapabilities)
+import Control.Concurrent.Async (async, wait)
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TMQueue
+import Control.Monad
+import Control.Monad.Loops (unfoldM)
 import Data.Either (rights)
 import Data.List (tails)
 import Data.Void (Void)
@@ -20,3 +28,31 @@ searchAll p = let parser = try p <|> (anyChar *> parser) in parser
 
 findAll :: Parser a -> String -> [a]
 findAll parser = rights . map (parse parser "") . init . tails
+
+multi :: forall a. (a -> a) -> (a -> Bool) -> Int -> a -> IO [a]
+multi nextToCheck isSolution numSolutions start = do
+  setNumCapabilities threads
+  current <- atomically $ newTVar start
+  solnsLeft <- atomically $ newTVar numSolutions
+  solutions <- atomically $ newTMQueue
+  ps <- replicateM threads $ async $ process current solnsLeft solutions
+  mapM_ wait ps
+  atomically $ closeTMQueue solutions
+  atomically $ unfoldM (readTMQueue solutions)
+    where bufferSize = 64
+          threads = 4
+          process :: TVar a -> TVar Int -> TMQueue a -> IO ()
+          process current solnsLeft solutions = go
+              where go = do
+                      sn <- readTVarIO solnsLeft
+                      when (sn > 0) $ do
+                        buffer <- atomically $ do
+                                x <- readTVar current
+                                let (xs, next:_) = splitAt bufferSize $ iterate nextToCheck x
+                                writeTVar current next
+                                return xs
+                        let solns = filter isSolution buffer
+                        when (not $ null solns) $ atomically $ do
+                          modifyTVar' solnsLeft (subtract $ length solns)
+                          forM_ solns $ writeTMQueue solutions
+                        go
--- src/Year2015/Day04.hs
+++ src/Year2015/Day04.hs
@@ -3,49 +3,19 @@ module Year2015.Day04
     , part2
     ) where
 
-import Control.Concurrent (setNumCapabilities)
-import Control.Concurrent.Async (async, wait)
-import Control.Concurrent.STM (TVar, atomically, modifyTVar', newTVar, readTVar, readTVarIO)
-import Control.Monad (replicateM)
-import Control.Monad.Extra (mapMaybeM)
+import Utils
+
 import Crypto.Hash.MD5 (hash)
 import Data.ByteString (ByteString, isPrefixOf, pack)
 import qualified Data.ByteString.Char8 as B
 import Data.Monoid ((<>))
-import Data.List (find)
 
 
-findHash :: (ByteString -> Bool) -> ByteString -> Int
-findHash f seed = head $ filter (f . hash . (seed <>) . B.pack . show) [0..]
-
-part1 :: String -> Int
-part1 = findHash f . B.pack
+part1 :: ByteString -> Int
+part1 seed = head $ filter f [0..]
     where zero5s = [pack [0,0,x] | x <- [0..15]]
-          f x = any (`isPrefixOf` x) zero5s
-
-process :: Int -> (Int -> Bool) -> TVar Int -> TVar Int -> IO (Maybe Int)
-process bufferSize cond current nSolutions = go
-    where go = do
-            sn <- readTVarIO nSolutions
-            if sn > 0 then do
-              cn <- atomically $ do
-                      x <- readTVar current
-                      modifyTVar' current (+bufferSize)
-                      return x
-              case find cond [cn..cn+bufferSize - 1] of
-                Just x -> do
-                  atomically $ modifyTVar' nSolutions pred
-                  return $ Just x
-                Nothing -> go
-            else return Nothing
+          f = (\x -> any (`isPrefixOf` x) zero5s) . hash . (seed <>) . B.pack . show
 
 part2 :: ByteString -> IO Int
-part2 seed = do
-  let bufferSize = 64
-      threads = 4
-  setNumCapabilities threads
-  current <- atomically $ newTVar 0
-  nSolutions <- atomically $ newTVar 1
-  ps <- replicateM threads $ async $ process bufferSize f current nSolutions
-  head <$> mapMaybeM wait ps
+part2 seed = head <$> multi (+1) f 1 0
     where f = (pack [0,0,0] `isPrefixOf`) . hash . (seed <>) . B.pack . show
