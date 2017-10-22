c868ea6b0d01838d3af4117f7c10134545e84075
Dan Seminara <semi225599@gmail.com>
2017-10-22 14:23:47 -0400

    Use conduit for 2016 Day 14

--- src/Year2016/Day14.hs
+++ src/Year2016/Day14.hs
@@ -3,48 +3,48 @@ module Year2016.Day14
     , part2
     ) where
 
+import Conduit
+import Control.Arrow
 import Data.Attoparsec.ByteString.Char8 (Parser, anyChar, char, parseOnly)
 import Data.ByteString.Base16 (encode)
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Conduit.List as L
 import Data.Either (rights)
 import Data.Maybe (listToMaybe)
-import Data.Sequence ((|>), (><), Seq, ViewR(..))
+import Data.Sequence ((|>), Seq)
 import qualified Data.Sequence as S
 import qualified Crypto.Hash.MD5 as MD5
 import Data.Monoid ((<>))
-import Data.Void (Void)
 
 
 hash :: ByteString -> ByteString
 hash = encode . MD5.hash
 
 findAll :: Parser a -> ByteString -> [a]
-findAll parser = rights . map (parseOnly parser) . init. B.tails
+findAll parser = rights . map (parseOnly parser) . init . B.tails
 
 find3 :: Parser Char
 find3 = anyChar >>= char >>= char
 
-find :: ByteString -> (ByteString -> ByteString) -> Int -> Int -> Seq Int
-find seed hash num n = go S.empty S.empty [0..]
-    where go :: Seq Int -> Seq (Int, Char) -> [Int] -> Seq Int
-          go c pot (x:xs)
-              | S.length c >= num = S.take num c
-              | otherwise =
-                  case threeInARow of
-                    Nothing -> go c pot' xs
-                    Just ch -> let (done, rest) = S.partition hasFive pot'
-                               in go (c >< fmap fst done) (rest |> (x, ch)) xs
-              where hashed = (!! n) . tail . iterate hash $ seed <> (B.pack $ show x)
-                    threeInARow = listToMaybe $ findAll find3 hashed
-                    pot' = S.dropWhileL ((>1000) . (x-) . fst) pot
-                    hasFive p = B.replicate 5 (snd p) `B.isInfixOf` hashed
-
-end :: Seq a -> a
-end = (\(_ :> r) -> r) . S.viewr
+find :: (Monad m) => ByteString -> (ByteString -> ByteString) -> Int -> Source m Int
+find seed hash n = L.sourceList (map (id &&& hashNum) [0..]) .| go S.empty
+    where hashNum = (!! n) . tail . iterate hash . (seed <>) . B.pack . show
+          go :: (Monad m) => Seq (Int, Char) -> Conduit (Int, ByteString) m Int
+          go pot = do
+            Just (i, hashed) <- await
+            let threeInARow = listToMaybe $ findAll find3 hashed
+                pot' = S.dropWhileL ((>1000) . (i-) . fst) pot
+                hasFive p = B.replicate 5 (snd p) `B.isInfixOf` hashed
+            case threeInARow of
+              Nothing -> go pot'
+              Just ch -> let (done, rest) = S.partition hasFive pot'
+                         in do
+                           yieldMany $ fmap fst done
+                           go $ rest |> (i, ch)
 
 part1 :: ByteString -> Int
-part1 s = end $ find s hash 64 0
+part1 s = last $ runConduitPure $ find s hash 0 .| L.take 64
 
 part2 :: ByteString -> Int
-part2 s = end $ find s hash 64 2016
+part2 s = last $ runConduitPure $ find s hash 2016 .| L.take 64
