64e33f24835df0c90cf3a96de43749778785ff2a
Dan Seminara <semi225599@gmail.com>
2017-12-01 02:25:21 -0500

    Use text

--- src/Year2017/Day01.hs
+++ src/Year2017/Day01.hs
@@ -4,19 +4,19 @@ module Year2017.Day01
     ) where
 
 import Data.Char (digitToInt)
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import qualified Data.Text as T
 
 
-rotate :: Int -> [a] -> [a]
-rotate _ [] = []
-rotate n xs = zipWith const (drop n (cycle xs)) xs
+captcha :: (Int -> Int) -> Text -> Int
+captcha f xs = let (end, start) = T.splitAt (f $ T.length xs) xs
+               in sum [ digitToInt a
+                      | (a, b) <- T.zip xs (start <> end)
+                      , a == b ]
 
-captcha :: Int -> [Int] -> Int
-captcha offset xs = sum $ zipWith f xs $ rotate offset xs
-    where f a b = if a == b then a else 0
+part1 :: Text -> Int
+part1 = captcha (const 1)
 
-part1 :: String -> Int
-part1 = captcha 1 . map digitToInt
-
-part2 :: String -> Int
-part2 input = let digits = map digitToInt input
-              in captcha (length digits `div` 2) digits
+part2 :: Text -> Int
+part2 = captcha (`div` 2)
