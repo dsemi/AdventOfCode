5dc9b09e62323d33adad437cf946c80dfb50e316
Dan Seminara <semi225599@gmail.com>
2017-12-06 01:48:13 -0500

    Cleanup 2017 Day 6

--- src/Year2017/Day06.hs
+++ src/Year2017/Day06.hs
@@ -4,30 +4,27 @@ module Year2017.Day06
     ) where
 
 import Control.Monad
-import qualified Data.HashMap.Strict as M
-import Data.Vector (Vector)
+import Data.HashMap.Strict (empty, insert, member, (!))
 import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector.Mutable as M
+
 
 data Cycle = Cycle { lengthToCycle :: Int
                    , lengthOfCycle :: Int
                    }
 
-parseBlocks :: String -> Vector Int
-parseBlocks = V.fromList . map read . words
-
 redistributeUntilCycle :: String -> Cycle
-redistributeUntilCycle = go 0 M.empty . parseBlocks
+redistributeUntilCycle = go 0 empty . V.fromList . map read . words
     where go c m v
-              | M.member serialized m = Cycle c (c - m M.! serialized)
-              | otherwise = go (c+1) (M.insert serialized c m) $ redistribute v
+              | member serialized m = Cycle c $ c - m ! serialized
+              | otherwise = go (c+1) (insert serialized c m) $ V.modify redistribute v
               where serialized = show v
-          redistribute v = V.modify redist v
-              where i = V.maxIndex v
-                    val = v V.! i
-                    redist v = do
-                      MV.write v i 0
-                      forM_ (map (`mod` MV.length v) [i+1 .. i+val]) $ MV.modify v (+1)
+          redistribute v = do
+            i <- V.maxIndex <$> V.unsafeFreeze v
+            val <- M.read v i
+            M.write v i 0
+            forM_ (map (`mod` M.length v) [i+1 .. i+val]) $
+                M.modify v (+1)
 
 part1 :: String -> Int
 part1 = lengthToCycle . redistributeUntilCycle
