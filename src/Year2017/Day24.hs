56743a07469fcf9a52bbeb151c9f1488248f8abd
Dan Seminara <semi225599@gmail.com>
2018-01-03 21:40:15 -0500

    Cleanup 2017 Day 24

--- src/Year2017/Day24.hs
+++ src/Year2017/Day24.hs
@@ -1,39 +1,40 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Year2017.Day24
     ( part1
     , part2
     ) where
 
 import Control.Arrow
-import Data.List (delete, maximumBy)
+import Data.List (delete)
 import Data.List.Split
-import Data.Ord
 
 
-data Bridge = Bridge { strength :: !Int, len :: !Int, pins :: Int }
-data Pipe = Pipe { a :: Int, b :: Int } deriving (Eq)
+type Pipe = (Int, Int)
+type Bridge = (Int, [Pipe])
 
 connect :: Bridge -> Pipe -> Maybe Bridge
-connect (Bridge {..}) (Pipe {..})
-    | pins == a = Just $ Bridge (strength + a + b) (len + 1) b
-    | pins == b = Just $ Bridge (strength + a + b) (len + 1) a
+connect (pins, pipes) pipe@(a, b)
+    | pins == a = Just (b, pipe : pipes)
+    | pins == b = Just (a, pipe : pipes)
     | otherwise = Nothing
 
 parsePipes :: String -> [Pipe]
 parsePipes = map parse . lines
-    where parse inp = let [x, y] = splitOn "/" inp
-                      in Pipe (read x) (read y)
+    where parse = f . splitOn "/"
+          f [x, y] = (read x, read y)
+          f _ = error "Invalid pipe"
 
-combos :: Bridge -> [Pipe] -> [Bridge]
-combos bridge [] = [bridge]
-combos bridge ps = [ bridge'' | pipe <- ps
-                   , Just bridge' <- pure $ connect bridge pipe
-                   , bridge'' <- bridge' : combos bridge' (delete pipe ps)
-                   ]
+combos :: [Pipe] -> [[Pipe]]
+combos = map snd . go (0, [])
+    where go br [] = [ br ]
+          go br ps = [ x | pipe <- ps
+                     , Just br' <- [connect br pipe]
+                     , x <- br' : go br' (delete pipe ps) ]
+
+strength :: [Pipe] -> Int
+strength = sum . map (uncurry (+))
 
 part1 :: String -> Int
-part1 = maximum . map strength . combos (Bridge 0 0 0) . parsePipes
+part1 = maximum . map strength . combos . parsePipes
 
 part2 :: String -> Int
-part2 = strength . maximumBy (comparing (len &&& strength)) . combos (Bridge 0 0 0) . parsePipes
+part2 = snd . maximum . map (length &&& strength) . combos . parsePipes
