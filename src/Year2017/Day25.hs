b4ac661ae58a770c5bc6611c21a9b9ab39e92018
Dan Seminara <semi225599@gmail.com>
2018-08-04 22:33:19 -0400

    Use tape struct

--- src/Year2017/Day25.hs
+++ src/Year2017/Day25.hs
@@ -7,34 +7,40 @@ module Year2017.Day25
 
 import Utils
 
-import Control.Monad.ST
 import Data.Either.Utils (fromEither)
 import Data.HashMap.Strict (HashMap, (!))
 import qualified Data.HashMap.Strict as M
-import qualified Data.HashTable.Class as H
-import qualified Data.HashTable.ST.Basic as B
 import Data.Maybe
 import Text.Megaparsec (eitherP, parseMaybe, sepBy)
 import Text.Megaparsec.Char (anyChar, char, string)
 import Text.Megaparsec.Char.Lexer (decimal, signed)
 
 
-data Rule = Rule { valueToWrite :: Int
-                 , dirFun :: Int -> Int
-                 , nextState :: Char
-                 }
-type Tape s = B.HashTable s Int Int
-data Machine s = Machine { tape :: Tape s
-                         , cursor :: !Int
-                         , state :: !Char
-                         , rules :: HashMap (Char, Int) Rule
-                         }
+data Rule = Rule Int (Tape -> Tape) Char
+data Tape = Tape [Int] Int [Int]
+data Machine = Machine { tape :: Tape
+                       , state :: !Char
+                       , rules :: HashMap (Char, Int) Rule
+                       }
 
-parseMachine :: String -> ST s (Int, Machine s)
-parseMachine input = do
-  let (steps, start, rules) = fromJust $ parseMaybe parse input
-  tp <- H.new
-  pure (steps, Machine tp 0 start rules)
+left :: Tape -> Tape
+left (Tape [] v ys)     = Tape [] 0 (v:ys)
+left (Tape (x:xs) v ys) = Tape xs x (v:ys)
+
+right :: Tape -> Tape
+right (Tape xs v [])     = Tape (v:xs) 0 []
+right (Tape xs v (y:ys)) = Tape (v:xs) y ys
+
+get :: Tape -> Int
+get (Tape _ v _) = v
+
+assign :: Int -> Tape -> Tape
+assign v (Tape xs _ ys) = Tape xs v ys
+
+parseMachine :: String -> (Int, Machine)
+parseMachine input =
+    let (steps, start, rules) = fromJust $ parseMaybe parse input
+    in (steps, Machine (Tape [] 0 []) start rules)
     where parse = do
             start <- string "Begin in state " *> anyChar <* string ".\n"
             steps <- string "Perform a diagnostic checksum after " *> int <* string " steps.\n\n"
@@ -48,23 +54,22 @@ parseMachine input = do
           parseRule c = do
             i <- string "  If the current value is " *> int <* string ":\n"
             v <- string "    - Write the value " *> int <* string ".\n"
-            dir <- string "    - Move one slot to the " *> eitherP (string "left." *> pure pred)
-                                                                   (string "right." *> pure succ)
+            dir <- string "    - Move one slot to the " *> eitherP (string "left." *> pure left)
+                                                                   (string "right." *> pure right)
             ns <- string "\n    - Continue with state " *> anyChar <* char '.'
             return $ ((c, i), Rule v (fromEither dir) ns)
           int = signed (return ()) decimal
 
-step :: Int -> Machine s -> ST s Int
-step 0 (Machine {tape}) = do
-  H.foldM (\a (_, v) -> pure $ a + v) 0 tape
-step n (Machine {tape, cursor, state, rules}) = do
-  val <- fromMaybe 0 <$> H.lookup tape cursor
-  let Rule {valueToWrite, dirFun, nextState} = rules ! (state, val)
-  H.insert tape cursor valueToWrite
-  step (n-1) $ Machine tape (dirFun cursor) nextState rules
+step :: Int -> Machine -> Machine
+step 0 m = m
+step n (Machine {tape,state,rules}) =
+    let (Rule v f state') = rules ! (state, get tape)
+    in step (n-1) $ Machine (f (assign v tape)) state' rules
 
 part1 :: String -> Int
-part1 input = runST $ parseMachine input >>= uncurry step
+part1 input = let (n, machine) = parseMachine input
+                  (Tape xs v ys) = tape $ step n machine
+              in v + sum xs + sum ys
 
 part2 :: String -> String
 part2 = const ""
