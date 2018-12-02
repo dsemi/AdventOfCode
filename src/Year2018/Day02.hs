677c20e671844a98a3e9b3565de6213fa122e6a0
Dan Seminara <semi225599@gmail.com>
2018-12-02 00:39:10 -0500

    2018 Day 2

--- src/Year2018/Day02.hs
+++ src/Year2018/Day02.hs
@@ -3,9 +3,21 @@ module Year2018.Day02
     , part2
     ) where
 
+import Data.List (group, sort, tails)
 
-part1 :: String -> String
-part1 = id
+
+part1 :: String -> Int
+part1 input = length (filter (any (==2)) letterCounts)
+              * length (filter (any (==3)) letterCounts)
+    where letterCounts = map (map length . group . sort) $ lines input
+
 
 part2 :: String -> String
-part2 = const ""
+part2 = head . concatMap findAlmostMatching . tails . lines
+    where keepCommon [] _ = []
+          keepCommon _ [] = []
+          keepCommon (x:xs) (y:ys)
+              | x == y = x : keepCommon xs ys
+              | otherwise = keepCommon xs ys
+          findAlmostMatching [] = error "Can't find almost matching IDs"
+          findAlmostMatching (x:xs) = filter ((== length x - 1) . length) $ map (keepCommon x) xs
