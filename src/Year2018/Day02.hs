b499ee1f57ce3ece4e4c5e064eaa5d7e3d5114ba
Dan Seminara <semi225599@gmail.com>
2018-12-03 13:21:53 -0500

    2018 Day 3

--- src/Year2018/Day02.hs
+++ src/Year2018/Day02.hs
@@ -12,7 +12,6 @@ part1 input = length (filter (any (==2)) letterCounts)
               * length (filter (any (==3)) letterCounts)
     where letterCounts = map (map length . group . sort) $ lines input
 
-
 part2 :: String -> String
 part2 = head . concatMap findAlmostMatching . tails . lines
     where keepCommon xs ys = catMaybes $ zipWith (\x y -> if x == y then Just x else Nothing) xs ys
--- src/Year2018/Day03.hs
+++ src/Year2018/Day03.hs
@@ -3,9 +3,42 @@ module Year2018.Day03
     , part2
     ) where
 
+import Utils
 
-part1 :: String -> String
-part1 = id
+import Data.Ix
+import Data.Array.Unboxed
+import Data.Maybe
+import Text.Megaparsec (parseMaybe, sepBy)
+import Text.Megaparsec.Char (char, string)
+import Text.Megaparsec.Char.Lexer (decimal)
 
-part2 :: String -> String
-part2 = const ""
+
+type Coord = (Int, Int)
+data Claim = Claim { num :: Int
+                   , rect :: (Coord, Coord)
+                   }
+
+parseClaims :: String -> [Claim]
+parseClaims = map (fromJust . parseMaybe parse) . lines
+    where parse :: Parser Claim
+          parse = do
+            n <- char '#' *> decimal
+            [x, y] <- string " @ " *> (decimal `sepBy` char ',')
+            [w, h] <- string ": " *> (decimal `sepBy` char 'x')
+            pure $ Claim n ((x, y), (x+w-1, y+h-1))
+
+coordFreq :: [Claim] -> UArray (Int, Int) Int
+coordFreq claims = accumArray (+) 0 ((0, 0), (upperX, upperY)) $ zip ranges $ repeat 1
+    where upperX = maximum $ map (fst . snd . rect) claims
+          upperY = maximum $ map (snd . snd . rect) claims
+          ranges = concatMap (range . rect) claims
+
+part1 :: String -> Int
+part1 = length . filter (>1) . elems . coordFreq . parseClaims
+
+findNonOverlappingClaim :: [Claim] -> Claim
+findNonOverlappingClaim claims = head $ filter (all ((==1) . (grid !)) . range . rect) claims
+    where grid = coordFreq claims
+
+part2 :: String -> Int
+part2 = num . findNonOverlappingClaim . parseClaims
