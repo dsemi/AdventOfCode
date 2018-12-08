aa3f79eeef26b337e92b552588ffe4561678d365
Dan Seminara <semi225599@gmail.com>
2018-12-08 17:28:24 -0500

    2018 Day 8

--- src/Year2018/Day08.hs
+++ src/Year2018/Day08.hs
@@ -3,9 +3,28 @@ module Year2018.Day08
     , part2
     ) where
 
+import Utils
 
-part1 :: String -> String
-part1 = id
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Tree
+import Text.Megaparsec (count, parseMaybe)
+import Text.Megaparsec.Char (space)
+import Text.Megaparsec.Char.Lexer (decimal)
 
-part2 :: String -> String
-part2 = const ""
+
+parseNodes :: String -> Tree [Int]
+parseNodes = fromJust . parseMaybe parse
+    where parse :: Parser (Tree [Int])
+          parse = do
+            n <- space *> decimal
+            m <- space *> decimal
+            flip Node <$> count n parse <*> count m (space *> decimal)
+
+part1 :: String -> Int
+part1 = sum . fmap sum . parseNodes
+
+part2 :: String -> Int
+part2 = go . parseNodes
+    where go (Node metadata []) = sum metadata
+          go (Node metadata children) =
+              sum $ map (fromMaybe 0 . fmap go . flip lookup (zip [1..] children)) metadata
