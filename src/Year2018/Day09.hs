b7efa569773e738d4512a9a69b12cc6ac12dc82b
Dan Seminara <semi225599@gmail.com>
2018-12-09 13:02:15 -0500

    2018 Day 9

--- src/Year2018/Day09.hs
+++ src/Year2018/Day09.hs
@@ -1,11 +1,31 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Year2018.Day09
     ( part1
     , part2
     ) where
 
+import Control.Lens hiding (re)
+import Data.List.PointedList.Circular
+import qualified Data.IntMap.Strict as M
+import Text.Regex.PCRE.Heavy
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+parse :: String -> (Int, Int)
+parse = (\[a, b] -> (a, b)) . map read . snd . head . scan regex
+    where regex = [re|(\d+) players; last marble is worth (\d+) points|]
+
+play :: (Int, Int) -> Maybe Int
+play (n, s) = go 1 M.empty (singleton 0)
+    where go p m c
+              | p == s+1 = Just $ maximum $ M.elems m
+              | p `rem` 23 /= 0 = go (p+1) m $ insertRight p (next c)
+              | otherwise = let c' = moveN (-7) c
+                                m' = M.insertWith (+) (p `rem` n) (p + c' ^. focus) m
+                            in delete c' >>= go (p+1) m'
+
+part1 :: String -> Maybe Int
+part1 = play . parse
+
+part2 :: String -> Maybe Int
+part2 = play . (_2 *~ 100) . parse
