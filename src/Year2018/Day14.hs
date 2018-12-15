767b5ab92fb3f139a8c7a5d57ad3d6bd08747db9
Dan Seminara <semi225599@gmail.com>
2018-12-14 19:19:36 -0500

    2018 Day 14

--- src/Year2018/Day14.hs
+++ src/Year2018/Day14.hs
@@ -3,9 +3,30 @@ module Year2018.Day14
     , part2
     ) where
 
+import Data.Char
+import Data.List (isPrefixOf, tails)
+import Data.Sequence ((><))
+import qualified Data.Sequence as S
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+digits :: Int -> [Int]
+digits n
+    | n < 10 = [n]
+    | otherwise = [1, n `rem` 10]
+
+recipes :: String
+recipes = '3' : '7' : go 0 1 (S.fromList [3, 7])
+    where go elf1 elf2 rs =
+              map intToDigit newRs
+                      ++ go (succ (score elf1 + elf1) `rem` S.length rs')
+                             (succ (score elf2 + elf2) `rem` S.length rs')
+                             rs'
+              where score = S.index rs
+                    newRs = digits $ score elf1 + score elf2
+                    rs' = rs >< S.fromList newRs
+
+part1 :: Int -> String
+part1 n = take 10 $ drop n $ recipes
+
+part2 :: String -> Int
+part2 n = length $ takeWhile (not . (n `isPrefixOf`)) $ tails recipes
