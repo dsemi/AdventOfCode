d38ad81c95412d62cdb3631654b5240382b6ac80
Dan Seminara <semi225599@gmail.com>
2018-12-24 22:37:07 -0500

    Cleanup Day 24

--- src/Year2018/Day24.hs
+++ src/Year2018/Day24.hs
@@ -7,21 +7,21 @@ module Year2018.Day24
 
 import Control.Arrow
 import Control.Lens
+import Control.Monad.Extra
 import Control.Monad.State
-import Data.List (maximumBy, partition, sortBy)
+import Data.List (maximumBy, sortBy)
 import Data.List.Split (splitOn)
 import Data.Maybe
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as M
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as S
 import Data.Ord
-import Data.Tuple
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
-import Debug.Trace
 
 
-type Key = Int
 data Group = Group { _name :: String
                    , _numUnits :: Int
                    , _hitpts :: Int
@@ -33,15 +33,12 @@ data Group = Group { _name :: String
                    } deriving (Eq, Ord, Show)
 makeLenses ''Group
 
-data Battle = Battle { _attacks :: [(Key, Key)]
-                     , _groups :: Map Key Group
-                     } deriving (Eq, Show)
-makeLenses ''Battle
+type Battle = IntMap Group
 
 effPwr :: Group -> Int
 effPwr g = g^.numUnits * g^.dmg
 
-parseGroups :: String -> Map Key Group
+parseGroups :: String -> Battle
 parseGroups = M.fromList . zip [0..]
               . concatMap (fromJust . parseMaybe army) . splitOn "\n\n"
     where army = do
@@ -51,17 +48,15 @@ parseGroups = M.fromList . zip [0..]
           group n = do
             u <- decimal <* string " units each with "
             hp <- decimal <* string " hit points "
-            (ws, is) <- fromMaybe ([], []) <$> optional (between (char '(') (string ") ") wAndI)
+            ms <- fromMaybe []
+                  <$> optional (between (char '(') (string ") ") (modifiers `sepBy` string "; "))
             d <- string "with an attack that does " *> decimal <* spaceChar
             t <- many letterChar <* string " damage at initiative "
             i <- decimal
-            pure $ Group n u hp d t i ws is
-          weakness = string "weak to " *> some letterChar `sepBy` string ", "
-          immunity = string "immune to " *> some letterChar `sepBy` string ", "
-          wAndI = try ((,) <$> weakness <*> (string "; " *> immunity))
-                  <|> try (swap <$> ((,) <$> immunity <*> (string "; " *> weakness)))
-                  <|> try ((,[]) <$> weakness)
-                  <|> try (([],) <$> immunity)
+            pure $ Group n u hp d t i (find "weak" ms) (find "immune" ms)
+          find k = fromMaybe [] . lookup k
+          modifiers = (,) <$> (some letterChar <* string " to ")
+                      <*> (some letterChar `sepBy` string ", ")
 
 calcDmg :: Group -> Group -> Int
 calcDmg g1 g2
@@ -69,65 +64,59 @@ calcDmg g1 g2
     | g1^.type' `elem` g2^.immunities = 0
     | otherwise = effPwr g1
 
-selectTarget :: (MonadState Battle m) => Group -> m (Maybe Key)
-selectTarget grp = do
-  attacked <- map snd <$> use attacks
-  poss <- filter (\(i, g) -> grp^.name /= g^.name && i `notElem` attacked) . M.toList <$> use groups
-  let dmgs = map (\(i, g) -> (i, calcDmg grp g, effPwr g, g^.initiative)) poss
-      max' = maximumBy (comparing (\(_, dm, pwr, ini) -> (dm, pwr, ini))) dmgs
-  if null poss || view _2 max' == 0
-  then pure Nothing
-  else pure $ Just $ view _1 max'
+selectTarget :: (MonadState Battle m) => IntSet -> Group -> m (Maybe Int)
+selectTarget attacked grp = do
+  dmgs <- map (\(i, g) -> (i, calcDmg grp g, effPwr g, g^.initiative))
+          . filter (\(i, g) -> grp^.name /= g^.name && S.notMember i attacked) . M.toList <$> get
+  let max' = maximumBy (comparing (\(_, dm, pwr, ini) -> (dm, pwr, ini))) dmgs
+  pure $ if null dmgs || view _2 max' == 0
+         then Nothing
+         else Just $ view _1 max'
 
-targetSelection :: (MonadState Battle m) => m ()
+targetSelection :: (MonadState Battle m) => m [(Int, Int)]
 targetSelection = do
-  gs <- sortBy (comparing (over both negate . (effPwr &&& _initiative) . snd))
-        . M.toList <$> use groups
-  forM_ gs $ \(i, g) -> do
-    t <- selectTarget g
-    when (isJust t) $ attacks %= ((i, fromJust t):)
-  gps <- use groups
-  attacks %= sortBy (comparing (negate . _initiative . (gps M.!) . fst))
-  pure ()
+  groups <- get
+  iniSort groups <$> go S.empty (targetSort (M.toList groups))
+    where targetSort = sortBy (comparing (over both negate . (effPwr &&& _initiative) . snd))
+          iniSort groups = sortBy (comparing (negate . _initiative . (groups M.!) . fst))
+          go _ [] = pure []
+          go s ((i, g) : rest) =
+              selectTarget s g >>= \case
+                           Just t -> ((i, t) :) <$> go (S.insert t s) rest
+                           Nothing -> go s rest
 
-attack :: (MonadState Battle m) => m ()
-attack = do
-  atks <- use attacks
+attack :: (MonadState Battle m) => [(Int, Int)] -> m ()
+attack atks = do
   forM_ atks $ \(k1, k2) -> do
-    gps <- use groups
-    when (M.member k1 gps) $ do
-      let g1 = gps M.! k1
-          g2 = gps M.! k2
-          d = calcDmg g1 g2
-      if d > g2^.hitpts * g2^.numUnits
-      then groups %= M.delete k2
-      else groups %= M.adjust (numUnits %~ (subtract (d `div` g2^.hitpts))) k2
-  attacks .= []
+    whenM (M.member k1 <$> get) $ do
+      g1 <- fromJust <$> use (at k1)
+      g2 <- fromJust <$> use (at k2)
+      let unitsLeft = g2^.numUnits - calcDmg g1 g2 `div` g2^.hitpts
+      if unitsLeft <= 0
+      then modify' $ M.delete k2
+      else modify' $ M.adjust (numUnits .~ unitsLeft) k2
 
-battle :: (MonadState Battle m) => m ()
-battle = get >>= go
-    where go s = do
-            gps <- M.elems <$> use groups
-            when (not (all (head gps^.name ==) (map _name gps))) $ do
-              targetSelection
-              attack
-              s' <- get
-              when (s /= s') $ go s'
-
-numUnitsLeft :: Map Key Group -> Int
-numUnitsLeft = sum . map _numUnits . M.elems . _groups . execState battle . Battle []
+battle :: (MonadState Battle m) => m Bool
+battle = do
+  s <- get
+  let gps = M.elems s
+  if any (head gps^.name /=) (map _name gps)
+  then do
+    targetSelection >>= attack
+    ifM ((s /=) <$> get) battle (pure False)
+  else pure True
 
 part1 :: String -> Int
-part1 = numUnitsLeft . parseGroups
+part1 = sum . map _numUnits . M.elems . execState battle . parseGroups
 
-boostImmune :: Int -> Map Key Group -> Map Key Group
+boostImmune :: Int -> Battle -> Battle
 boostImmune n = M.map (\g -> if g^.name == "Immune System" then g & dmg +~ n else g)
 
 part2 :: String -> Int
-part2 input = let gps = parseGroups input
-              in head $ filter (>0)
-                     $ map (\n -> let (imms, infs) = partition ((== "Immune System") . _name)
-                                                     $ M.elems $ _groups $ execState battle
-                                                     $ Battle [] $ boostImmune n gps
-                                      ns = sum $ map _numUnits imms
-                                  in if null infs then ns else 0) [0..]
+part2 input = let groups = parseGroups input
+              in head $ [ sum' | n <- [0..]
+                        , let (b, s) = runState battle $ boostImmune n groups
+                        , let sum' = sum $ map _numUnits $ filter ((== "Immune System") . _name)
+                                     $ M.elems s
+                        , b && sum' > 0
+                        ]
