f13a5a045c27e119028f0872741d8e75b421fdbb
Dan Seminara <semi225599@gmail.com>
2018-12-25 01:22:56 -0500

    2018 Day 25

--- src/Year2018/Day25.hs
+++ src/Year2018/Day25.hs
@@ -1,11 +1,41 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Year2018.Day25
     ( part1
     , part2
     ) where
 
+import Control.Lens
+import Data.Maybe
+import Data.List (partition)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
 
-part1 :: String -> String
-part1 = id
+
+type Point = (Int, Int, Int, Int)
+
+parsePoints :: String -> [Point]
+parsePoints = fromJust . parseMaybe @() ((do
+                [a, b, c, d] <- signed (pure ()) decimal `sepBy` char ','
+                pure (a, b, c, d)) `sepBy` newline)
+
+dist :: Point -> Point -> Int
+dist (w0, x0, y0, z0) (w1, x1, y1, z1) =
+    abs (w0 - w1) + abs (x0 - x1) + abs (y0 - y1) + abs (z0 - z1)
+
+constellations :: [Point] -> [[Point]]
+constellations [] = []
+constellations pts = let (constellation, rest) = go [head pts] (tail pts)
+                     in constellation : constellations rest
+    where go ps [] = (ps, [])
+          go [] rest = ([], rest)
+          go ps ps' =
+              let (neighbs, rest) = partition (\p -> any ((<= 3) . dist p) ps) ps'
+              in over _1 (ps ++) $ go neighbs rest
+
+part1 :: String -> Int
+part1 = length . constellations . parsePoints
 
 part2 :: String -> String
 part2 = const ""
