7517f8132d073799a58dee96030163204a811339
Dan Seminara <semi225599@gmail.com>
2019-12-04 00:34:35 -0500

    2019 Day 4

--- src/Year2019/Day04.hs
+++ src/Year2019/Day04.hs
@@ -1,15 +1,33 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Year2019.Day04
     ( part1
     , part2
     ) where
+
+import Data.Char (digitToInt)
+import Data.Ix (range)
+import Data.List (group)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
 
 import DaysTH
 
 
 $(buildProb)
 
-part1' :: String -> String
-part1' = const ""
+parseRange :: String -> Maybe (Int, Int)
+parseRange = parseMaybe @() ((,) <$> decimal <* char '-' <*> decimal)
 
-part2' :: String -> String
-part2' = const""
+numValid :: (Int -> Bool) -> String -> Maybe Int
+numValid f = fmap (length . filter cond . range) . parseRange
+    where cond num = and (zipWith (<=) digits (tail digits))
+                     && any f (map length (group digits))
+              where digits = map digitToInt $ show num
+
+part1' :: String -> Maybe Int
+part1' = numValid (>=2)
+
+part2' :: String -> Maybe Int
+part2' = numValid (==2)
