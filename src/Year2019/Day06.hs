66757e7ed5414f300beff9ff789e3720378ea82b
Dan Seminara <semi225599@gmail.com>
2019-12-06 01:40:39 -0500

    2019 Day 6

--- src/Year2019/Day06.hs
+++ src/Year2019/Day06.hs
@@ -3,13 +3,27 @@ module Year2019.Day06
     , part2
     ) where
 
+import Control.Arrow
+import qualified Data.Map.Strict as M
+import Data.List (unfoldr)
+import Data.Tuple
+
 import DaysTH
+import Utils
 
 
 $(buildProb)
 
-part1' :: String -> String
-part1' = const ""
+parseOrbits :: String -> [(String, String)]
+parseOrbits = map ((id *** tail) . break (==')')) . lines
 
-part2' :: String -> String
-part2' = const""
+part1' :: String -> Int
+part1' = sum . map fst . bfs' . M.fromListWith (++) . map (id *** (:[])) . parseOrbits
+    where bfs' orbitMap = bfs "COM" (\x -> M.findWithDefault [] x orbitMap)
+
+part2' :: String -> Int
+part2' input = transfers "YOU" "SAN"
+    where invTree = map swap $ parseOrbits input
+          pathFromCom obj = reverse $ unfoldr (fmap (id &&& id) . (`lookup` invTree)) obj
+          transfers a b = go (pathFromCom a) (pathFromCom b)
+          go xs ys = length xs + length ys - 2 * length (takeWhile id (zipWith (==) xs ys))
