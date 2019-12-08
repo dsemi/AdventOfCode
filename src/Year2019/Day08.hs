c05f26ae379d90bd1eee1baf5a4d7291b3698a5c
Dan Seminara <semi225599@gmail.com>
2019-12-08 00:50:36 -0500

    2019 Day 8

--- src/Year2019/Day08.hs
+++ src/Year2019/Day08.hs
@@ -3,9 +3,16 @@ module Year2019.Day08
     , part2
     ) where
 
+import Data.List (minimumBy, transpose)
+import Data.List.Split (chunksOf)
+import Data.Ord (comparing)
 
-part1 :: String -> String
-part1 = const ""
+
+part1 :: String -> Int
+part1 = solve . minimumBy (comparing $ count '0') . chunksOf 150
+    where count n = length . filter (==n)
+          solve layer =  count '1' layer * count '2' layer
 
 part2 :: String -> String
-part2 = const ""
+part2 = draw . map (head . filter (/= '2')) . transpose . chunksOf 150
+    where draw = ('\n':) . unlines . chunksOf 25 . map (\case '0' -> ' '; _ -> '#')
