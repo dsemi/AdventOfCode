126a3f9310b2c5e3f22080bfb166a6c96e0e68c3
Dan Seminara <semi225599@gmail.com>
2019-12-10 00:15:32 -0500

    Cleanup intcode

--- src/Year2019/IntCode.hs
+++ src/Year2019/IntCode.hs
@@ -6,10 +6,10 @@ module Year2019.IntCode
     ) where
 
 import Data.Bool
-import Data.Char
 import Data.Functor.Identity
 import Data.List.Split (splitOn)
-import Data.IntMap.Strict (IntMap)
+import Data.Maybe
+import Data.IntMap.Strict (IntMap, (!))
 import qualified Data.IntMap.Strict as M
 import Pipes
 import qualified Pipes.Prelude as P
@@ -28,57 +28,48 @@ parse = M.fromList . zip [0..] . map read . splitOn ","
 build :: Memory -> Program
 build = Program 0 0
 
+data Instr = Add | Mul | Sav | Out | Jit | Jif | Lt | Eql | Arb | Hlt
+
+toInstr :: Int -> Instr
+toInstr i = fromMaybe (error $ "Unknown op code: " ++ show i)
+            $ lookup i [ (1, Add), (2, Mul), (3, Sav), (4, Out), (5, Jit)
+                       , (6, Jif), (7,  Lt), (8, Eql), (9, Arb), (99, Hlt) ]
+
 data Mode = Pos | Imm | Rel deriving (Enum)
-
-getModes :: Int -> [Mode]
-getModes = (++ repeat Pos) . map (toEnum . digitToInt) . reverse . show
-
-(!) :: Program -> Int -> Int
-(!) prog k = M.findWithDefault 0 k $ memory prog
 
 run :: (Monad m) => Program -> Pipe Int Int m Program
 run prog =
-    case instr of
-      -- Addition
-      1  -> run $ inc 4 $ set 3 (val 1 + val 2) prog
-      -- Multiplication
-      2  -> run $ inc 4 $ set 3 (val 1 * val 2) prog
-      -- Store
-      3  -> await >>= \v -> run $ inc 2 $ set 1 v prog
-      -- Print
-      4  -> yield (val 1) >> run (inc 2 prog)
-      -- Jump if true
-      5  -> run $ bool (inc 3) (jmp $ val 2) (val 1 /= 0) prog
-      -- Jump if false
-      6  -> run $ bool (inc 3) (jmp $ val 2) (val 1 == 0) prog
-      -- Less than
-      7  -> run $ inc 4 $ set 3 (bool 0 1 $ val 1 < val 2) prog
-      -- Equal
-      8  -> run $ inc 4 $ set 3 (bool 0 1 $ val 1 == val 2) prog
-      -- Adjust relative base
-      9  -> run $ inc 2 $ incRb (val 1) prog
-      -- Stop execution
-      99 -> pure prog
-      x  -> error $ "Unknown instruction: " ++ show x
-    where arg n = prog ! (idx prog + n)
+    case toInstr op of
+      Add -> run $ inc 4 $ set 3 (val 1 + val 2) prog
+      Mul -> run $ inc 4 $ set 3 (val 1 * val 2) prog
+      Sav -> await >>= \v -> run $ inc 2 $ set 1 v prog
+      Out -> yield (val 1) >> run (inc 2 prog)
+      Jit -> run $ bool (inc 3) (jmp $ val 2) (val 1 /= 0) prog
+      Jif -> run $ bool (inc 3) (jmp $ val 2) (val 1 == 0) prog
+      Lt  -> run $ inc 4 $ set 3 (bool 0 1 $ val 1 < val 2) prog
+      Eql -> run $ inc 4 $ set 3 (bool 0 1 $ val 1 == val 2) prog
+      Arb -> run $ inc 2 $ incRb (val 1) prog
+      Hlt -> pure prog
+    where arg n = get $ idx prog + n
           inc n p = p {idx=idx p + n}
           jmp n p = p {idx=n}
           incRb n p = p {relBase=relBase p + n}
-          set a v p = case modes !! (a - 1) of
+          get i = M.findWithDefault 0 i $ memory prog
+          set a v p = case mode a of
                         Pos -> p {memory=M.insert (arg a) v (memory p)}
                         Imm -> error "set on immediate"
                         Rel -> p {memory=M.insert (arg a + relBase p) v (memory p)}
-          val a = case modes !! (a - 1) of
-                    Pos -> prog ! arg a
+          val a = case mode a of
+                    Pos -> get $ arg a
                     Imm -> arg a
-                    Rel -> prog ! (arg a + relBase prog)
-          instr = (prog ! idx prog) `mod` 100
-          modes = getModes $ (prog ! idx prog) `div` 100
+                    Rel -> get $ arg a + relBase prog
+          op = get (idx prog) `mod` 100
+          mode i = toEnum $ get (idx prog) `div` 10^(i+1) `mod` 10
 
 runNoIO :: Int -> Int -> Memory -> Int
-runNoIO a b mem = (! 0) $ runIdentity $ runEffect
-                $ (error "No input") >-> run prog >-> P.drain
+runNoIO a b mem = (! 0) $ memory $ runIdentity $ runEffect
+                  $ (error "No input") >-> run prog >-> P.drain
     where prog = build $ M.insert 1 a $ M.insert 2 b mem
 
 runWithInput :: [Int] -> Memory -> [Int]
-runWithInput input mem = P.toList $ each input >-> void (run (build mem))
+runWithInput input mem = P.toList $ each input >-> void (run $ build mem)
