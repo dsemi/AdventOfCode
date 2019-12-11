2b105bb774559f885933c2431f712ee7a830f6f2
Dan Seminara <semi225599@gmail.com>
2019-12-10 21:22:17 -0500

    Use V2 for 2019 Day 10

--- src/Year2019/Day10.hs
+++ src/Year2019/Day10.hs
@@ -10,37 +10,34 @@ import Data.List (delete, maximumBy, sortBy)
 import Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as M
 import Data.Ord (comparing)
+import Linear.V2
 
 
-parse :: String -> [(Int, Int)]
-parse input = [ (x, y) | (y, line) <- zip [0..] $ lines input
+parse :: String -> [V2 Int]
+parse input = [ V2 x y | (y, line) <- zip [0..] $ lines input
               , (x, v) <- zip [0..] line
               , v == '#'
               ]
 
-data Point = Point { cartesian :: (Int, Int)
-                   , theta :: Double
-                   , len :: Double
-                   }
+theta :: V2 Int -> V2 Int -> Double
+theta v1 v2 = atan2 (-x) y
+    where V2 x y = fmap fromIntegral $ v2 - v1
 
--- Polar coord starting at (0, -1) (where -y is up) and rotating clockwise
-relPoint :: (Int, Int) -> (Int, Int) -> Point
-relPoint (x1, y1) (x2, y2) = Point (x2, y2) (atan2 (-x') y') $ sqrt (x'^2 + y'^2)
-    where y' = fromIntegral $ y2 - y1
-          x' = fromIntegral $ x2 - x1
+dist :: V2 Int -> V2 Int -> Int
+dist v1 v2 = sum $ abs $ v2 - v1
 
-visibilityMap :: (Int, Int) -> [(Int, Int)] -> Map Double [Point]
-visibilityMap pt = M.map (sortBy (comparing len)) . M.fromListWith (++)
-                   . map ((theta &&& (:[])) . relPoint pt) . delete pt
+visibilityMap :: V2 Int -> [V2 Int] -> Map Double [V2 Int]
+visibilityMap pt = M.map (sortBy $ comparing $ dist pt) . M.fromListWith (++)
+                   . map (theta pt &&& (:[])) . delete pt
 
-maxDetected :: [(Int, Int)] -> Map Double [Point]
+maxDetected :: [V2 Int] -> Map Double [V2 Int]
 maxDetected asts = maximumBy (comparing M.size) $ map (`visibilityMap` asts) asts
 
 part1 :: String -> Int
 part1 = M.size . maxDetected . parse
 
 part2 :: String -> Int
-part2 = (\(a, b) -> 100 * a + b). cartesian . go 200 . maxDetected . parse
+part2 = (\(V2 a b) -> 100 * a + b) . go 200 . maxDetected . parse
     where go c m
               | M.null m = error "empty"
               | length keys >= c = head $ m ! (keys !! (c-1))
