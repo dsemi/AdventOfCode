3b19ad1bf9186ffd5eb5e27c9d0a3a163f47045e
Dan Seminara <semi225599@gmail.com>
2019-12-19 18:50:31 -0500

    2019 Day 19

--- src/Year2019/Day19.hs
+++ src/Year2019/Day19.hs
@@ -3,9 +3,23 @@ module Year2019.Day19
     , part2
     ) where
 
+import Year2019.IntCode
 
-part1 :: String -> String
-part1 = const ""
 
-part2 :: String -> String
-part2 = const ""
+isPulled :: Memory -> Int -> Int -> Bool
+isPulled mem x y = (== 1) $ head $ runWithInput [x, y] mem
+
+part1 :: String -> Int
+part1 (parse -> mem) = length $ filter id $ isPulled mem <$> [0..49] <*> [0..49]
+
+findSquare :: Memory -> Int -> Int -> (Int, Int)
+findSquare mem xSize ySize = go 0 0
+    where go x y
+              | isPulled mem (x + xSize - 1) y = (x, y)
+              | otherwise = go x' (y+1)
+              where x' | isPulled mem x (y + ySize) = x
+                       | otherwise = x + 1
+
+part2 :: String -> Int
+part2 (parse -> mem) = x * 10000 + y
+    where (x, y) = findSquare mem 100 100
