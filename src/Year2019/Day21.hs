55c4867616c69b765afa0ec422fee8838a79607e
Dan Seminara <semi225599@gmail.com>
2019-12-21 02:42:50 -0500

    2019 Day 21

--- src/Year2019/Day21.hs
+++ src/Year2019/Day21.hs
@@ -3,9 +3,31 @@ module Year2019.Day21
     , part2
     ) where
 
+import Data.Char
 
-part1 :: String -> String
-part1 = const ""
+import Year2019.IntCode
 
-part2 :: String -> String
-part2 = const ""
+run :: [String] -> String -> Int
+run instrs = last . runWithInput (map ord $ unlines instrs) . parse
+
+part1 :: String -> Int
+part1 = run [ "OR A T"
+            , "AND B T"
+            , "AND C T"
+            , "NOT T J"
+            , "AND D J"
+            , "WALK"
+            ]
+
+part2 :: String -> Int
+part2 = run [ "OR A T"
+            , "AND B T"
+            , "AND C T"
+            , "NOT T J"
+            , "AND D J"
+            , "NOT J T"
+            , "OR E T"
+            , "OR H T"
+            , "AND T J"
+            , "RUN"
+            ]
