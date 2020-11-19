0681404dbef218e7113664c12adba22147ffdbb7
Dan Seminara <semi225599@gmail.com>
2020-11-19 15:19:05 -0500

    Add structure for 2020

--- src/Days.hs
+++ src/Days.hs
@@ -131,5 +131,30 @@ import Year2019.Day22
 import Year2019.Day23
 import Year2019.Day24
 import Year2019.Day25
+import Year2020.Day01
+import Year2020.Day02
+import Year2020.Day03
+import Year2020.Day04
+import Year2020.Day05
+import Year2020.Day06
+import Year2020.Day07
+import Year2020.Day08
+import Year2020.Day09
+import Year2020.Day10
+import Year2020.Day11
+import Year2020.Day12
+import Year2020.Day13
+import Year2020.Day14
+import Year2020.Day15
+import Year2020.Day16
+import Year2020.Day17
+import Year2020.Day18
+import Year2020.Day19
+import Year2020.Day20
+import Year2020.Day21
+import Year2020.Day22
+import Year2020.Day23
+import Year2020.Day24
+import Year2020.Day25
 
 $(buildProbs)
---
+++ src/Year2020/Day07.hs
@@ -0,0 +1,11 @@
+module Year2020.Day07
+    ( part1
+    , part2
+    ) where
+
+
+part1 :: String -> String
+part1 = id
+
+part2 :: String -> String
+part2 = const ""
