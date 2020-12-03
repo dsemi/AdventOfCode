b3e9a68face5a09ee8002e962b91d5f2aca36fef
Dan Seminara <semi225599@gmail.com>
2020-12-03 00:53:39 -0500

    2020 Day 3

--- src/Year2020/Day03.hs
+++ src/Year2020/Day03.hs
@@ -4,8 +4,18 @@ module Year2020.Day03
     ) where
 
 
-part1 :: String -> String
-part1 = id
+countTrees :: Int -> Int -> String -> Int
+countTrees right down = go . map cycle . lines
+    where go [] = 0
+          go xs = n + go (map (drop right) (drop down xs))
+              where n = if head (head xs) == '#' then 1 else 0
 
-part2 :: String -> String
-part2 = const ""
+part1 :: String -> Int
+part1 = countTrees 3 1
+
+part2 :: String -> Int
+part2 s = product [ countTrees 1 1 s
+                  , countTrees 3 1 s
+                  , countTrees 5 1 s
+                  , countTrees 7 1 s
+                  , countTrees 1 2 s ]
