479b59c527a4aa58d6fe1007cfd4c3d16368c6e9
Dan Seminara <semi225599@gmail.com>
2020-12-05 00:32:54 -0500

    2020 Day 5

--- src/Year2020/Day05.hs
+++ src/Year2020/Day05.hs
@@ -3,9 +3,19 @@ module Year2020.Day05
     , part2
     ) where
 
+import Data.List (sort)
+import Numeric (readInt)
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+seatIds :: String -> [Int]
+seatIds = map (fst . head . readInt 2 (`elem` "FBLR") f) . lines
+    where f d | d `elem` "BR" = 1
+              | d `elem` "FL" = 0
+              | otherwise = error "Bad parse"
+
+part1 :: String -> Int
+part1 = maximum . seatIds
+
+part2 :: String -> Int
+part2 = (+1) . fst . head . findGap . sort . seatIds
+    where findGap x = filter (\(a, b) -> a + 2 == b) $ zip x $ tail x
