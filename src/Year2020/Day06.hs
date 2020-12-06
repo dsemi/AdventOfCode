2ad87a405a53b04bea9a2f2317dd9aff94bb823e
Dan Seminara <semi225599@gmail.com>
2020-12-06 01:34:27 -0500

    2020 Day 6

--- src/Year2020/Day06.hs
+++ src/Year2020/Day06.hs
@@ -3,9 +3,15 @@ module Year2020.Day06
     , part2
     ) where
 
+import Data.List (intersect, union)
+import Data.List.Split
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+solve :: (String -> String -> String) -> String -> Int
+solve f = sum . map (length . foldr1 f . words) . splitOn "\n\n"
+
+part1 :: String -> Int
+part1 = solve union
+
+part2 :: String -> Int
+part2 = solve intersect
