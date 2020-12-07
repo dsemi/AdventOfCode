b4aa1ce3f5142b34070d4007fe59e56c3cab0cb0
Dan Seminara <semi225599@gmail.com>
2020-12-07 01:54:24 -0500

    2020 Day 7

--- src/Year2020/Day07.hs
+++ src/Year2020/Day07.hs
@@ -3,9 +3,28 @@ module Year2020.Day07
     , part2
     ) where
 
+import Data.Map (Map, (!))
+import qualified Data.Map as M
+import Data.Maybe
+import Text.Megaparsec
+import Text.Megaparsec.Char.Lexer
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+parseBags :: String -> Map String [(Int, String)]
+parseBags = M.fromList . map (fromJust . parseMaybe parser) . lines
+    where bag = manyTill anySingle $
+                try (chunk " bag" >> optional (single 's') >> optional (single '.'))
+          parser :: Parsec () String (String, [(Int, String)])
+          parser = do
+            key <- manyTill anySingle (try $ chunk " bags contain ")
+            vals <- try (chunk "no other bags." *> pure [])
+                    <|> (((,) <$> decimal <* single ' ' <*> bag) `sepBy` chunk ", ")
+            pure (key, vals)
+
+part1 :: String -> Int
+part1 (parseBags -> bags) = length $ filter holdsShinyGold $ M.keys bags
+    where holdsShinyGold = any (\(_, k) -> k == "shiny gold" || holdsShinyGold k) . (bags !)
+
+part2 :: String -> Int
+part2 (parseBags -> bags) = countBags "shiny gold"
+    where countBags = sum . map (\(n, k) -> n + n * countBags k) . (bags !)
