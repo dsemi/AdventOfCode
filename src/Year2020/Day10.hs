2c7159e3eae2bc99b1c7b9e9ecdcf171dbd4f1b3
Dan Seminara <semi225599@gmail.com>
2020-12-10 01:10:07 -0500

    2020 Day 10

--- src/Year2020/Day10.hs
+++ src/Year2020/Day10.hs
@@ -3,9 +3,27 @@ module Year2020.Day10
     , part2
     ) where
 
+import Data.Function.Memoize
+import Data.List (sort)
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+parse :: String -> [Int]
+parse input = 0 : (ns ++ [maximum ns + 3])
+    where ns = sort $ map read $ lines input
+
+joltz :: [Int] -> Int
+joltz ns = length (filter (==1) js) * length (filter (==3) js)
+    where js = zipWith (-) (tail ns) (ns)
+
+part1 :: String -> Int
+part1 = joltz . parse
+
+numValid :: [Int] -> Int
+numValid ns = memoFix go $ last ns
+    where go f n
+              | n < 0 || n `notElem` ns = 0
+              | n == 0 = 1
+              | otherwise = f (n-1) + f (n-2) + f (n-3)
+
+part2 :: String -> Int
+part2 = numValid . parse
