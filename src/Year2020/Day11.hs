ac7219047c85a846ba7e833c114df402721debf5
Dan Seminara <semi225599@gmail.com>
2020-12-11 00:49:02 -0500

    2020 Day 11

--- src/Year2020/Day11.hs
+++ src/Year2020/Day11.hs
@@ -3,9 +3,56 @@ module Year2020.Day11
     , part2
     ) where
 
+import Data.Array.Unboxed
+import Data.Maybe
+import Linear.V2
 
-part1 :: String -> String
-part1 = id
+type Coord = V2 Int
+type Grid = UArray Coord Char
 
-part2 :: String -> String
-part2 = const ""
+parse :: String -> Grid
+parse inp = array (fst (head grid), fst (last grid)) grid
+    where grid = [ (V2 x y, c) | (y, row) <- zip [0..] $ lines inp
+                 , (x, c) <- zip [0..] row ]
+
+adjacentsOccupied :: [Coord] -> Grid -> Int
+adjacentsOccupied adjs grid = length $ filter (=='#') $ map (grid !) adjs
+
+next :: Grid -> Coord -> (Coord, Char)
+next grid xy = (xy, v)
+    where v | grid ! xy == 'L' && numOcc == 0 = '#'
+            | grid ! xy == '#' && numOcc >= 4 = 'L'
+            | otherwise = grid ! xy
+          numOcc = adjacentsOccupied adjs grid
+          adjs = filter (inRange (bounds grid)) $ map (+xy) dirs
+          dirs = [ V2 (-1) (-1), V2 0 (-1), V2 1 (-1)
+                 , V2 (-1) 0, V2 1 0
+                 , V2 (-1) 1, V2 0 1, V2 1 1 ]
+
+stabilize :: (Grid -> Coord -> (Coord, Char)) -> Grid -> Int
+stabilize f grid = length $ filter (=='#') $ elems $ head $ catMaybes
+                   $ zipWith (\a b -> if a == b then Just a else Nothing) grids $ tail grids
+    where nextState :: Grid -> Grid
+          nextState g = array (bounds g) $ map (f g) $ indices grid
+          grids = iterate nextState grid
+
+part1 :: String -> Int
+part1 = stabilize next . parse
+
+next2 :: Grid -> Coord -> (Coord, Char)
+next2 grid xy = (xy, v)
+    where v | grid ! xy == 'L' && numOcc == 0 = '#'
+            | grid ! xy == '#' && numOcc >= 5 = 'L'
+            | otherwise = grid ! xy
+          numOcc = adjacentsOccupied adjs grid
+          adjs = [ head xs | dir <- dirs
+                 , let xs = dropWhile ((=='.') . (grid !))
+                            $ takeWhile (inRange (bounds grid))
+                            $ tail $ iterate (+dir) xy
+                 , not (null xs) ]
+          dirs = [ V2 (-1) (-1), V2 0 (-1), V2 1 (-1)
+                 , V2 (-1) 0, V2 1 0
+                 , V2 (-1) 1, V2 0 1, V2 1 1 ]
+
+part2 :: String -> Int
+part2 = stabilize next2 . parse
