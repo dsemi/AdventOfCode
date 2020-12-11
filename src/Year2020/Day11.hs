fa81e3b73a927f1e7270be15004565c704785794
Dan Seminara <semi225599@gmail.com>
2020-12-11 17:00:37 -0500

    Clean day 11

--- src/Year2020/Day11.hs
+++ src/Year2020/Day11.hs
@@ -19,34 +19,30 @@ dirs :: [Coord]
 dirs = [ V2 (-1) (-1), V2 0 (-1), V2 1 (-1)
        , V2 (-1) 0, V2 1 0, V2 (-1) 1, V2 0 1, V2 1 1 ]
 
-next :: Grid -> Coord -> (Coord, Char)
-next grid xy = (xy, v)
-    where v | grid ! xy == 'L' && numOcc == 0 = '#'
-            | grid ! xy == '#' && numOcc >= 4 = 'L'
-            | otherwise = grid ! xy
-          numOcc = length $ filter (=='#') $ map (grid !)
-                   $ filter (inRange (bounds grid)) $ map (+xy) dirs
+adjacents :: Grid -> Coord -> [Coord]
+adjacents grid xy = filter (inRange (bounds grid)) $ map (+xy) dirs
 
-stabilize :: (Grid -> Coord -> (Coord, Char)) -> Grid -> Int
-stabilize f grid = length $ filter (=='#') $ elems $ fst $ head
-                   $ filter (uncurry (==)) $ zip grids $ tail grids
+stabilize :: Int -> (Grid -> Coord -> [Coord]) -> Grid -> Int
+stabilize nThreshold adjs grid = length $ filter (=='#') $ elems $ fst $ head
+                                 $ filter (uncurry (==)) $ zip grids $ tail grids
     where nextState :: Grid -> Grid
-          nextState g = array (bounds g) $ map (f g) $ indices grid
+          nextState g = array (bounds g) $ map (next g) $ indices grid
+          next g xy = (xy, v)
+              where v | g ! xy == 'L' && numOcc == 0 = '#'
+                      | g ! xy == '#' && numOcc >= nThreshold = 'L'
+                      | otherwise = g ! xy
+                    numOcc = length $ filter (=='#') $ map (g !) $ adjs g xy
           grids = iterate nextState grid
 
 part1 :: String -> Int
-part1 = stabilize next . parse
+part1 = stabilize 4 adjacents . parse
 
-next2 :: Grid -> Coord -> (Coord, Char)
-next2 grid xy = (xy, v)
-    where v | grid ! xy == 'L' && numOcc == 0 = '#'
-            | grid ! xy == '#' && numOcc >= 5 = 'L'
-            | otherwise = grid ! xy
-          numOcc = length [ 1 | dir <- dirs
-                          , let xs = dropWhile ((=='.') . (grid !))
-                                     $ takeWhile (inRange (bounds grid))
-                                     $ tail $ iterate (+dir) xy
-                          , not (null xs) && grid ! head xs == '#' ]
+adjacents2 :: Grid -> Coord -> [Coord]
+adjacents2 grid xy = [ head xs | dir <- dirs
+                     , let xs = dropWhile ((=='.') . (grid !))
+                                $ takeWhile (inRange (bounds grid))
+                                $ tail $ iterate (+dir) xy
+                     , not (null xs) ]
 
 part2 :: String -> Int
-part2 = stabilize next2 . parse
+part2 = stabilize 5 adjacents2 . parse
