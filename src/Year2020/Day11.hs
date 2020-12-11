b4afd6fa8e83a9d04548a7b07dd8a2daccf1ccec
Dan Seminara <semi225599@gmail.com>
2020-12-11 00:56:08 -0500

    Day 11

--- src/Year2020/Day11.hs
+++ src/Year2020/Day11.hs
@@ -4,8 +4,8 @@ module Year2020.Day11
     ) where
 
 import Data.Array.Unboxed
-import Data.Maybe
 import Linear.V2
+
 
 type Coord = V2 Int
 type Grid = UArray Coord Char
@@ -15,23 +15,21 @@ parse inp = array (fst (head grid), fst (last grid)) grid
     where grid = [ (V2 x y, c) | (y, row) <- zip [0..] $ lines inp
                  , (x, c) <- zip [0..] row ]
 
-adjacentsOccupied :: [Coord] -> Grid -> Int
-adjacentsOccupied adjs grid = length $ filter (=='#') $ map (grid !) adjs
+dirs :: [Coord]
+dirs = [ V2 (-1) (-1), V2 0 (-1), V2 1 (-1)
+       , V2 (-1) 0, V2 1 0, V2 (-1) 1, V2 0 1, V2 1 1 ]
 
 next :: Grid -> Coord -> (Coord, Char)
 next grid xy = (xy, v)
     where v | grid ! xy == 'L' && numOcc == 0 = '#'
             | grid ! xy == '#' && numOcc >= 4 = 'L'
             | otherwise = grid ! xy
-          numOcc = adjacentsOccupied adjs grid
-          adjs = filter (inRange (bounds grid)) $ map (+xy) dirs
-          dirs = [ V2 (-1) (-1), V2 0 (-1), V2 1 (-1)
-                 , V2 (-1) 0, V2 1 0
-                 , V2 (-1) 1, V2 0 1, V2 1 1 ]
+          numOcc = length $ filter (=='#') $ map (grid !)
+                   $ filter (inRange (bounds grid)) $ map (+xy) dirs
 
 stabilize :: (Grid -> Coord -> (Coord, Char)) -> Grid -> Int
-stabilize f grid = length $ filter (=='#') $ elems $ head $ catMaybes
-                   $ zipWith (\a b -> if a == b then Just a else Nothing) grids $ tail grids
+stabilize f grid = length $ filter (=='#') $ elems $ fst $ head
+                   $ filter (uncurry (==)) $ zip grids $ tail grids
     where nextState :: Grid -> Grid
           nextState g = array (bounds g) $ map (f g) $ indices grid
           grids = iterate nextState grid
@@ -44,15 +42,11 @@ next2 grid xy = (xy, v)
     where v | grid ! xy == 'L' && numOcc == 0 = '#'
             | grid ! xy == '#' && numOcc >= 5 = 'L'
             | otherwise = grid ! xy
-          numOcc = adjacentsOccupied adjs grid
-          adjs = [ head xs | dir <- dirs
-                 , let xs = dropWhile ((=='.') . (grid !))
-                            $ takeWhile (inRange (bounds grid))
-                            $ tail $ iterate (+dir) xy
-                 , not (null xs) ]
-          dirs = [ V2 (-1) (-1), V2 0 (-1), V2 1 (-1)
-                 , V2 (-1) 0, V2 1 0
-                 , V2 (-1) 1, V2 0 1, V2 1 1 ]
+          numOcc = length [ 1 | dir <- dirs
+                          , let xs = dropWhile ((=='.') . (grid !))
+                                     $ takeWhile (inRange (bounds grid))
+                                     $ tail $ iterate (+dir) xy
+                          , not (null xs) && grid ! head xs == '#' ]
 
 part2 :: String -> Int
 part2 = stabilize next2 . parse
