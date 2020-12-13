488a9222b53cdf8f82b9c05e0563ef8504e4344b
Dan Seminara <semi225599@gmail.com>
2020-12-13 00:36:15 -0500

    2020 Day 13

--- src/Year2020/Day13.hs
+++ src/Year2020/Day13.hs
@@ -3,9 +3,24 @@ module Year2020.Day13
     , part2
     ) where
 
+import Data.List (minimumBy)
+import Data.List.Split (splitOn)
+import Data.Ord (comparing)
+import Math.NumberTheory.Moduli.Chinese (chineseRemainder)
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+parse :: String -> (Integer, [(Integer, Integer)])
+parse input = (read time, buses)
+    where [time, row2] = lines input
+          buses = [ (i, read b) | (i, b) <- zip [0..] $ splitOn "," row2
+                  , b /= "x" ]
+
+part1 :: String -> Integer
+part1 input = busNo * (actualTime - earliestTime)
+    where (earliestTime, buses) = parse input
+          (busNo, actualTime) = minimumBy (comparing snd)
+                                $ map ((\b -> (b, b * (earliestTime `div` b + 1))) . snd) buses
+
+part2 :: String -> Maybe Integer
+part2 input = chineseRemainder [ (-i, b) | (i, b) <- buses]
+    where buses = snd $ parse input
