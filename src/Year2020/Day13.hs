768fd428e008c58197fc27097a3e03f1ded7366d
Dan Seminara <semi225599@gmail.com>
2020-12-14 12:27:19 -0500

    2020 Day 14

--- src/Year2020/Day13.hs
+++ src/Year2020/Day13.hs
@@ -12,15 +12,13 @@ import Math.NumberTheory.Moduli.Chinese (chineseRemainder)
 parse :: String -> (Integer, [(Integer, Integer)])
 parse input = (read time, buses)
     where [time, row2] = lines input
-          buses = [ (i, read b) | (i, b) <- zip [0..] $ splitOn "," row2
+          buses = [ (-i, read b) | (i, b) <- zip [0..] $ splitOn "," row2
                   , b /= "x" ]
 
 part1 :: String -> Integer
-part1 input = busNo * (actualTime - earliestTime)
+part1 input = uncurry (*) $ minimumBy (comparing snd)
+              $ map ((\b -> (b, b - earliestTime `mod` b)) . snd) buses
     where (earliestTime, buses) = parse input
-          (busNo, actualTime) = minimumBy (comparing snd)
-                                $ map ((\b -> (b, b * (earliestTime `div` b + 1))) . snd) buses
 
 part2 :: String -> Maybe Integer
-part2 input = chineseRemainder [ (-i, b) | (i, b) <- buses]
-    where buses = snd $ parse input
+part2 = chineseRemainder . snd . parse
--- src/Year2020/Day14.hs
+++ src/Year2020/Day14.hs
@@ -3,9 +3,36 @@ module Year2020.Day14
     , part2
     ) where
 
+import Utils
 
-part1 :: String -> String
-part1 = id
+import Control.Lens
+import Data.Bits
+import Data.List (isPrefixOf)
+import qualified Data.Map as M
 
-part2 :: String -> String
-part2 = const ""
+
+parse :: String -> [([(Int, Char)], Int, Int)]
+parse = concat . foldr go [[]] . lines
+    where go _ [] = error "bad parse"
+          go line (x:xs)
+              | "mask" `isPrefixOf` line = [] : map (_1 .~ zip [35,34..] (last (words line))) x : xs
+              | otherwise = let [r, v] = findAllInts line
+                            in ((undefined, r, v) : x) : xs
+
+part1 :: String -> Int
+part1 = sum . M.elems . M.fromList . map process . parse
+    where process (mask, r, v) = (r, foldr go v mask)
+          go (i, m) v = case m of
+                          '1' -> setBit v i
+                          '0' -> clearBit v i
+                          _ -> v
+
+part2 :: String -> Int
+part2 = sum . M.elems . M.fromList . concatMap process . parse
+    where process (mask, r, v) = map (,v) $ go mask [0] r
+          go [] ns _ = ns
+          go ((i, m):ms) ns r = go ms ((\b v -> v*2 + b) <$> bits <*> ns) r
+              where bits = case m of
+                             '1' -> [1]
+                             '0' -> [fromEnum (testBit r i)]
+                             _ -> [0, 1]
