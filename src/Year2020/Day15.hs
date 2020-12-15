62dbc05086dc0eb75204c1cdd537cbfb5c49f21c
Dan Seminara <semi225599@gmail.com>
2020-12-15 02:13:50 -0500

    2020 Day 15

--- src/Year2020/Day15.hs
+++ src/Year2020/Day15.hs
@@ -3,9 +3,25 @@ module Year2020.Day15
     , part2
     ) where
 
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.List.Split
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+run :: Int -> String -> Int
+run n input = runST $ do
+  let inp = map read $ splitOn "," input
+  arr <- newArray (0, n) (-1) :: ST s (STUArray s Int Int)
+  forM_ (zip [1..] inp) $ \(i, v) -> writeArray arr v i
+  (\f -> foldM f 0 [length inp + 1 .. n-1]) $ \v turn -> do
+    val <- readArray arr v
+    let newV = if val == -1 then 0 else turn - val
+    writeArray arr v turn
+    pure newV
+
+part1 :: String -> Int
+part1 = run 2020
+
+part2 :: String -> Int
+part2 = run 30000000
