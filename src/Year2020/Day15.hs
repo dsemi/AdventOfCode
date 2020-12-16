ed1295f26049732dc701b37e39c0b5c41284a08e
Dan Seminara <semi225599@gmail.com>
2020-12-16 02:28:14 -0500

    2020 Day 16

--- src/Year2020/Day15.hs
+++ src/Year2020/Day15.hs
@@ -5,20 +5,18 @@ module Year2020.Day15
 
 import Control.Monad
 import Control.Monad.ST
-import Data.Array.ST
 import Data.List.Split
+import qualified Data.Vector.Unboxed.Mutable as V
 
 
 run :: Int -> String -> Int
 run n input = runST $ do
   let inp = map read $ splitOn "," input
-  arr <- newArray (0, n) (-1) :: ST s (STUArray s Int Int)
-  forM_ (zip [1..] inp) $ \(i, v) -> writeArray arr v i
+  arr <- V.replicate (n+1) (-1)
+  forM_ (zip [1..] inp) $ \(i, v) -> V.unsafeWrite arr v i
   (\f -> foldM f 0 [length inp + 1 .. n-1]) $ \v i -> do
-    val <- readArray arr v
-    let newV = if val == -1 then 0 else i - val
-    writeArray arr v i
-    pure newV
+    val <- V.unsafeRead arr v <* V.unsafeWrite arr v i
+    pure $ if val == -1 then 0 else i - val
 
 part1 :: String -> Int
 part1 = run 2020
--- src/Year2020/Day16.hs
+++ src/Year2020/Day16.hs
@@ -1,11 +1,42 @@
+{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+
 module Year2020.Day16
     ( part1
     , part2
     ) where
 
+import Data.Ix
+import Data.List ((\\), isPrefixOf)
+import Data.List.Split
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+type Rule = (String, [(Int, Int)])
+
+parse :: String -> ([Rule], [Int], [[Int]])
+parse input = let [rules, yours, others] = splitOn "\n\n" input
+              in ( [ (cls, [(a, b), (c, d)]) | line <- lines rules
+                   , let [cls, rest] = splitOn ": " line
+                   , let [[a, b], [c, d]] = map (map read . splitOn "-") $ splitOn " or " rest ]
+                 , map read $ splitOn "," $ last $ lines yours
+                 , map (map read . splitOn ",") $ tail $ lines others )
+
+fieldIsValid :: Int -> Rule -> Bool
+fieldIsValid field (_, rs) = any (`inRange` field) rs
+
+invalidValues :: [Rule] -> [Int] -> [Int]
+invalidValues rules ticket = filter (\t -> not $ any (fieldIsValid t) rules) ticket
+
+part1 :: String -> Int
+part1 (parse -> (rules, _, tickets)) = sum $ map (sum . invalidValues rules) tickets
+
+determineFields :: [Rule] -> [[Int]] -> [String]
+determineFields rules tix = map (fst . head) $ until (all ((==1) . length)) removeSingles
+                            $ foldr (zipWith (filter . fieldIsValid)) (replicate len rules) tix
+    where len = length $ head tix
+          removeSingles xs = let set = concat $ filter ((==1) . length) xs
+                             in map (\x -> if length x == 1 then x else x \\ set) xs
+
+part2 :: String -> Int
+part2 (parse -> (rules, yours, tix)) =
+    product $ map fst $ filter (("departure" `isPrefixOf`) . snd)
+    $ zip yours $ determineFields rules $ filter (null . invalidValues rules) tix
