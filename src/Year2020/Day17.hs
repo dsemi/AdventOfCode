3427249b3b6dbebc5239e3b37d65fcef74a9e48d
Dan Seminara <semi225599@gmail.com>
2020-12-17 14:07:15 -0500

    2020 Day 17

--- src/Year2020/Day17.hs
+++ src/Year2020/Day17.hs
@@ -3,9 +3,41 @@ module Year2020.Day17
     , part2
     ) where
 
+import Control.Lens
+import Data.Array.Unboxed
+import Linear.V3
+import Linear.V4
 
-part1 :: String -> String
-part1 = id
 
-part2 :: String -> String
-part2 = const ""
+type Grid f = UArray (f Int) Bool
+
+parse :: String -> Grid V3
+parse input = array bds grid
+    where grid = [ (V3 x y 0, v == '#')
+                 | (y, row) <- zip [0..] $ lines input
+                 , (x, v) <- zip [0..] row]
+          bds = (minimum (map fst grid), maximum (map fst grid))
+
+neighbors :: (Ix (f Int), Traversable f) => Grid f -> f Int -> Int
+neighbors grid coord = sum [ 1 | coord' <- traverse (\x -> [x-1, x, x+1]) coord
+                           , coord' /= coord
+                           , inRange (bounds grid) coord'
+                           , grid ! coord' ]
+
+step :: (Ix (f Int), Traversable f) => Grid f -> Grid f
+step grid = listArray bounds' $ map f $ range bounds'
+    where (a, b) = bounds grid
+          bounds' = (subtract 1 <$> a, (+1) <$> b)
+          getVal coord = inRange (a, b) coord && grid ! coord
+          f coord = count == 3 || getVal coord && count == 2
+              where count = neighbors grid coord
+
+countAfterSix :: (Ix (f Int), Traversable f) => Grid f -> Int
+countAfterSix = length . filter id . elems . (!! 6) . iterate step
+
+part1 :: String -> Int
+part1 = countAfterSix . parse
+
+part2 :: String -> Int
+part2 (parse -> grid) = countAfterSix grid'
+    where grid' = ixmap (both %~ (\(V3 x y z) -> V4 x y z 0) $ bounds grid) (^._xyz) grid
