bf64bc0deecaac6b9a3b14a091f176b622db7dc9
Dan Seminara <semi225599@gmail.com>
2020-12-19 20:28:42 -0500

    Use parsec

--- src/Year2020/Day19.hs
+++ src/Year2020/Day19.hs
@@ -1,38 +1,37 @@
-{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Year2020.Day19
     ( part1
     , part2
     ) where
 
-import Data.List.Split
-import Data.Map (Map, (!))
-import qualified Data.Map as M
+import Data.IntMap (IntMap, (!))
+import qualified Data.IntMap as M
+import Data.Maybe
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
 
 
-data Rule = Const Char | Rule [[Int]]
+type Rule = Either Char [[Int]]
 
-parse :: String -> (Map Int Rule, [String])
-parse input = let [rules, inp] = splitOn "\n\n" input
-              in ( M.fromList $ map f $ lines rules
-                 , lines inp)
-    where f x = let [k, v] = splitOn ": " x
-                in if head v == '"'
-                   then (read k, Const (v !! 1))
-                   else (read k, Rule $ map (map read . words) $ splitOn " | " v)
+parseInput :: String -> (IntMap Rule, [String])
+parseInput = fromJust . parseMaybe @() input
+    where input = (,) <$> (M.fromList <$> endBy rule "\n") <* "\n" <*> (some lowerChar `sepBy` "\n")
+          rule = (,) <$> decimal <* ": "
+                 <*> eitherP ("\"" *> anySingle <* "\"") ((decimal `sepEndBy` " ") `sepBy` "| ")
 
-matches :: Map Int Rule -> String -> Bool
-matches rules = any null . go (rules M.! 0)
-    where go :: Rule -> String -> [String]
+solve :: [(Int, Rule)] -> String -> Int
+solve extras input = length $ filter (any null . go (rss ! 0)) strs
+    where (rules, strs) = parseInput input
+          rss = M.fromList extras `M.union` rules
           go _ [] = []
-          go (Const c) (x:xs) = if c == x then [xs] else []
-          go (Rule rs) xs = concatMap (foldl (\ys r -> concatMap (go r) ys) [xs] . map (rules !)) rs
+          go (Left c) (x:xs) = if c == x then [xs] else []
+          go (Right rs) xs = concatMap (foldl (\ys r -> concatMap (go r) ys) [xs] . map (rss !)) rs
 
 part1 :: String -> Int
-part1 (parse -> (rules, strs)) = length $ filter (matches rules) strs
+part1 = solve []
 
 part2 :: String -> Int
-part2 (parse -> (rules, strs)) = length $ filter (matches rules') strs
-    where rules' = M.fromList [ (8, Rule [[42], [42, 8]])
-                              , (11, Rule [[42, 31], [42, 11, 31]]) ]
-                   `M.union` rules
+part2 = solve [ (8, Right [[42], [42, 8]])
+              , (11, Right [[42, 31], [42, 11, 31]]) ]
