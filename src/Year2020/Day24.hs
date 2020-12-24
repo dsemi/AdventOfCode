03d9ce5dafc616361fc526d07479a7c5eb1c4897
Dan Seminara <semi225599@gmail.com>
2020-12-24 02:27:52 -0500

    Use parsec

--- src/Year2020/Day24.hs
+++ src/Year2020/Day24.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NegativeLiterals #-}
+{-# LANGUAGE NegativeLiterals, OverloadedStrings #-}
 
 module Year2020.Day24
     ( part1
@@ -7,12 +7,12 @@ module Year2020.Day24
 
 import Data.Bits
 import Data.Char
-import Linear.V3
+import Data.Maybe
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as M
+import Linear.V3
+import Text.Megaparsec
 
-
-data Dir = E | SE | SW | W | NW | NE deriving (Enum, Read)
 
 dirs :: [V3 Int -> V3 Int]
 dirs = [ (+ V3  1 -1  0) -- East
@@ -23,19 +23,13 @@ dirs = [ (+ V3  1 -1  0) -- East
        , (+ V3  1  0 -1) -- Northeast
        ]
 
-parse :: String -> [[V3 Int -> V3 Int]]
-parse = map (map ((dirs !!) . fromEnum) . go) . lines
-    where go :: String -> [Dir]
-          go [] = []
-          go (x:xs)
-              | x `elem` "ew" = read [toUpper x] : go xs
-              | otherwise = read (map toUpper [x, head xs]) : go (tail xs)
-
-flipTiles :: [[V3 Int -> V3 Int]] -> HashMap (V3 Int) Bool
+flipTiles :: String -> HashMap (V3 Int) Bool
 flipTiles = M.filter id . M.fromListWith xor . map ((,True) . foldr ($) (V3 0 0 0) . reverse)
+            . map (fromJust . parseMaybe @() pDirs) . lines . map toUpper
+    where pDirs = some $ choice $ zipWith (<$) dirs ["E", "SE", "SW", "W", "NW", "NE"]
 
 part1 :: String -> Int
-part1 = M.size . flipTiles . parse
+part1 = M.size . flipTiles
 
 step :: HashMap (V3 Int) Bool -> HashMap (V3 Int) Bool
 step m = M.filter id $ M.mapWithKey black adj
@@ -44,4 +38,4 @@ step m = M.filter id $ M.mapWithKey black adj
           neighbors x = map ($ x) dirs
 
 part2 :: String -> Int
-part2 = M.size . (!! 100) . iterate step . flipTiles . parse
+part2 = M.size . (!! 100) . iterate step . flipTiles
