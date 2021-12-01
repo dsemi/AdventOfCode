e1b211aa668220d009d8ad0151d20bfa4961072a
Dan Seminara <semi225599@gmail.com>
2021-12-01 00:14:23 -0500

    2021 Day 1

--- src/Year2021/Day01.hs
+++ src/Year2021/Day01.hs
@@ -4,8 +4,13 @@ module Year2021.Day01
     ) where
 
 
-part1 :: String -> String
-part1 = id
+part1 :: String -> Int
+part1 input = length $ filter id $ zipWith (<) ns $ tail ns
+    where ns :: [Int]
+          ns = map read $ lines input
 
-part2 :: String -> String
-part2 = const ""
+part2 :: String -> Int
+part2 input = length $ filter id $ zipWith (<) ns' $ tail ns'
+    where ns :: [Int]
+          ns = map read $ lines input
+          ns' = zipWith3 (\a b c -> a + b + c) ns (tail ns) (drop 2 ns)
