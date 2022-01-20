fcf0b1ac743768793b9531632271abaf5b2dded1
Dan Seminara <semi225599@gmail.com>
2022-01-20 17:48:10 -0500

    Use char directly

--- src/Year2021/Day06.hs
+++ src/Year2021/Day06.hs
@@ -3,11 +3,9 @@ module Year2021.Day06
     , part2
     ) where
 
-import Data.Char
-
 solve :: Int -> String -> Int
 solve n s = sum $ take 9 $ drop n ms
-    where ns = take 9 $ drop 7 $ cycle $ map (\x -> length $ filter (== intToDigit x) s) [0..8]
+    where ns = take 9 $ drop 7 $ cycle $ map (\x -> length $ filter (==x) s) ['0'..'8']
           ms = ns ++ zipWith (+) ms (drop 2 ms)
 
 part1 :: String -> Int
