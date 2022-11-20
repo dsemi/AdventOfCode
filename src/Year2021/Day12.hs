a85f062afbb208e4ac6482dc415751841f2fa7f1
Dan Seminara <semi225599@gmail.com>
2022-11-20 16:20:11 -0500

    State monad is slow

--- src/Year2021/Day12.hs
+++ src/Year2021/Day12.hs
@@ -3,10 +3,10 @@ module Year2021.Day12
     , part2
     ) where
 
-import Control.Monad.State.Strict
 import Data.Char
 import Data.HashMap.Strict ((!), HashMap)
 import qualified Data.HashMap.Strict as M
+import qualified Data.HashSet as S
 import Data.List.Split (splitOn)
 
 parse :: String -> HashMap String [String]
@@ -15,19 +15,13 @@ parse = M.fromListWith (++) . concatMap f . lines
                    in [(k, [v]), (v, [k])]
 
 dfs :: Bool -> HashMap String [String] -> Int
-dfs double m = evalState (go "start" double) M.empty
-    where go :: String -> Bool -> State (HashMap String Int) Int
-          go "end" _ = pure 1
-          go k dbl = do
-            vis <- get
-            let b = all isLower k && M.findWithDefault 0 k vis > 0
-            if b && (dbl || k == "start")
-            then pure 0
-            else do
-              modify' $ M.insertWith (+) k 1
-              s <- sum <$> mapM (\child -> go child (dbl || b)) (m ! k)
-              modify' $ M.adjust (subtract 1) k
-              pure s
+dfs double m = go double S.empty "start"
+    where go _ _ "end" = 1
+          go dbl vis k
+              | b && (dbl || k == "start") = 0
+              | otherwise = let vis' = S.insert k vis
+                            in sum $ map (go (dbl || b) vis') (m ! k)
+              where b = all isLower k && S.member k vis
 
 part1 :: String -> Int
 part1 = dfs True . parse
