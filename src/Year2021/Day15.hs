5cfc75c80b1933d60a93bb90f229cff9d02b36eb
Dan Seminara <semi225599@gmail.com>
2022-11-25 15:59:35 -0500

    Mutable impl for grid search

--- src/Year2021/Day15.hs
+++ src/Year2021/Day15.hs
@@ -3,18 +3,17 @@ module Year2021.Day15
     , part2
     ) where
 
+import Control.Lens ((^.))
+import Control.Monad
+import Control.Monad.Extra
+import Control.Monad.ST
 import Data.Array.Unboxed
 import Data.Char
-import Data.Graph.AStar
-import Data.HashSet (HashSet)
-import qualified Data.HashSet as S
+import Data.Sequence ((|>))
+import qualified Data.Sequence as S
+import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector.Unboxed.Mutable as MVU
 import Linear.V2
-
-neighbors :: UArray (V2 Int) Int -> V2 Int -> HashSet (V2 Int)
-neighbors grid = S.fromList . go
-    where bds = bounds grid
-          go pos = [ p | p <- map (pos+) [V2 1 0, V2 0 1, V2 (-1) 0, V2 0 (-1)]
-                   , inRange bds p ]
 
 parse :: String -> UArray (V2 Int) Int
 parse input = array (V2 0 0, V2 (rows-1) (cols-1))
@@ -23,15 +22,33 @@ parse input = array (V2 0 0, V2 (rows-1) (cols-1))
     where rows = length $ lines input
           cols = length $ head $ lines input
 
-part1 :: String -> Maybe Int
-part1 input = sum . map (grid !) <$> aStar (neighbors grid) (const (grid !))
-              (sum . abs . subtract goal) (== goal) (V2 0 0)
-    where grid = parse input
-          goal = snd $ bounds grid
+dijkstra :: UArray (V2 Int) Int -> Int
+dijkstra grid = runST $ do
+ lkup <- MVU.replicate (dim * dim) 0
+ forM_ (assocs grid) $ \(V2 r c, v) -> MVU.write lkup (dim*(r+1)+c+1) v
+ q <- MV.replicate 16 S.empty
+ MV.modify q (|> dim + 1) 0
+ go lkup q 0
+    where dim = snd (bounds grid) ^. _x + 3
+          goal = dim * dim - dim - 2
+          go lkup q qi =
+              MV.exchange q (qi `mod` 16) S.empty >>= foldM fld Nothing >>= maybe (go lkup q (qi+1)) pure
+              where fld Nothing p
+                        | p == goal = pure $ Just qi
+                        | otherwise = do
+                      unlessM ((<1) <$> MVU.read lkup p) $ do
+                        MVU.modify lkup (*(-1)) p
+                        forM_ [p-1, p+1, p-dim, p+dim] $ \n -> do
+                          v <- MVU.read lkup n
+                          when (v >= 1) $ MV.modify q (|> n) ((qi + v) `mod` 16)
+                      pure Nothing
+                    fld x _ = pure x
 
-part2 :: String -> Maybe Int
-part2 input = sum . map (grid !) <$> aStar (neighbors grid) (const (grid !))
-              (sum . abs . subtract goal) (== goal) (V2 0 0)
+part1 :: String -> Int
+part1 = dijkstra . parse
+
+part2 :: String -> Int
+part2 input = dijkstra grid
     where expand g = array (V2 0 0, V2 (mr*5-1) (mc*5-1))
                      [ (V2 r c, v) | r <- [0..mr*5-1]
                      , c <- [0..mc*5-1]
@@ -40,4 +57,3 @@ part2 input = sum . map (grid !) <$> aStar (neighbors grid) (const (grid !))
                      , let v = (g ! V2 rm cm - 1 + rd + cd) `mod` 9 + 1]
               where V2 mr mc = snd (bounds g) + V2 1 1
           grid = expand $ parse input
-          goal = snd $ bounds grid
