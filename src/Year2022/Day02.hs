cf2fa8161911f50eba68769468967b535b6a4535
Dan Seminara <semi225599@gmail.com>
2022-12-02 11:59:03 -0500

    Shorten

--- src/Year2022/Day02.hs
+++ src/Year2022/Day02.hs
@@ -6,18 +6,12 @@ module Year2022.Day02
 import Data.Char
 
 solve :: (Int -> Int -> Int) -> String -> Int
-solve conv = sum . map play . lines
-    where play [p1, _, p2] =
-              let a = ord p1 - ord 'A'
-                  b = conv (ord p2 - ord 'X') a
-                  wld | b == (a + 1) `mod` 3 = 6
-                      | b == a = 3
-                      | otherwise = 0
-              in wld + b + 1
+solve f = sum . map play . lines
+    where play [p1, _, p2] = f (ord p1 - ord 'A') (ord p2 - ord 'X')
           play _ = error "Malformed input"
 
 part1 :: String -> Int
-part1 = solve const
+part1 = solve $ \a b -> (b - a + 1) `mod` 3 * 3 + b + 1
 
 part2 :: String -> Int
-part2 = solve $ \c a -> (a + c - 1) `mod` 3
+part2 = solve $ \a b -> (a + b - 1) `mod` 3 + 3*b + 1
