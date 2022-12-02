4c32c4c4fa131aa205c20af2427e5783f642efd6
Dan Seminara <semi225599@gmail.com>
2022-12-02 01:22:47 -0500

    Shorten

--- src/Year2022/Day02.hs
+++ src/Year2022/Day02.hs
@@ -5,31 +5,19 @@ module Year2022.Day02
 
 import Data.Char
 
-beat :: Int -> Int
-beat = (`mod` 3) . succ
-
-lose :: Int -> Int
-lose = (`mod` 3) . pred
-
-solve :: (Char -> Int -> Int) -> String -> Int
+solve :: (Int -> Int -> Int) -> String -> Int
 solve conv = sum . map play . lines
     where play [p1, _, p2] =
               let a = ord p1 - ord 'A'
-                  b = conv p2 a
-                  wld | b == beat a = 6
-                      | b == lose a = 0
-                      | otherwise = 3
+                  b = conv (ord p2 - ord 'X') a
+                  wld | b == (a + 1) `mod` 3 = 6
+                      | b == a = 3
+                      | otherwise = 0
               in wld + b + 1
           play _ = error "Malformed input"
 
 part1 :: String -> Int
-part1 = solve $ \case
-                'X' -> const 0
-                'Y' -> const 1
-                'Z' -> const 2
+part1 = solve const
 
 part2 :: String -> Int
-part2 = solve $ \case
-                'X' -> lose
-                'Y' -> id
-                'Z' -> beat
+part2 = solve $ \c a -> (a + c - 1) `mod` 3
