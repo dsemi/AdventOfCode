e1501b60ba0764e5092803740e9b0beb1eef3852
Dan Seminara <semi225599@gmail.com>
2022-12-04 02:34:02 -0500

    Use parsec

--- src/Year2022/Day04.hs
+++ src/Year2022/Day04.hs
@@ -3,15 +3,17 @@ module Year2022.Day04
     , part2
     ) where
 
-import Data.List.Split
+import Data.Maybe
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer (decimal)
 
-solve :: (Int -> Int -> Int -> Int -> Bool) -> String -> Int
-solve f = length . filter (check . map (map read . splitOn "-") . splitOn ",") . lines
-    where check [[a0, a1], [b0, b1]] = f a0 a1 b0 b1
-          check _ = error "Malformed input"
+solve :: ((Int, Int, Int, Int) -> Bool) -> String -> Int
+solve f = length . filter (f . fromJust . parseMaybe @() p) . lines
+    where p = (,,,) <$> decimal <* char '-' <*> decimal <* char ',' <*> decimal <* char '-' <*> decimal
 
 part1 :: String -> Int
-part1 = solve $ \a0 a1 b0 b1 -> a0 <= b0 && a1 >= b1 || b0 <= a0 && b1 >= a1
+part1 = solve $ \(a0, a1, b0, b1) -> a0 <= b0 && a1 >= b1 || b0 <= a0 && b1 >= a1
 
 part2 :: String -> Int
-part2 = solve $ \a0 a1 b0 b1 -> a0 <= b1 && b0 <= a1
+part2 = solve $ \(a0, a1, b0, b1) -> a0 <= b1 && b0 <= a1
