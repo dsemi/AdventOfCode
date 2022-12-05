8e5cda3d5a7dff2394908a125ce54c8e5ea677dc
Dan Seminara <semi225599@gmail.com>
2022-12-05 09:31:47 -0500

    2022 Day 5

---
+++ src/Year2022/Day05.hs
@@ -0,0 +1,34 @@
+module Year2022.Day05
+    ( part1
+    , part2
+    ) where
+
+import Data.Char
+import Data.List (foldl')
+import Data.List.Split
+import Data.HashMap.Strict (HashMap, (!))
+import qualified Data.HashMap.Strict as M
+
+import Utils
+
+moveStacks :: Bool -> String -> String
+moveStacks inOrder input = map head . M.elems . foldl' move (makeStacks crates) $ lines instrs
+    where [s, instrs] = splitOn "\n\n" input
+          crates = lines s
+          num = (length (head crates) + 1) `div` 4
+          makeStacks :: [String] -> HashMap Int [Char]
+          makeStacks = M.fromList . zip [1..] . foldr (zipWith f) (replicate num []). map (go . tail)
+          f Nothing xs = xs
+          f (Just x) xs = x:xs
+          go [] = []
+          go (x:xs) = (if isUpper x then Just x else Nothing) : go (drop 3 xs)
+          move stacks instr = M.adjust (toMove ++) b $ M.insert a newStack stacks
+              where [n, a, b] = findAllInts instr
+                    (tm, newStack) = splitAt n $ stacks ! a
+                    toMove = if inOrder then tm else reverse tm
+
+part1 :: String -> String
+part1 = moveStacks False
+
+part2 :: String -> String
+part2 = moveStacks True
