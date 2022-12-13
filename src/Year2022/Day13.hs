3c011837aac1a8970556e875f4e7ee649ac0bba8
Dan Seminara <semi225599@gmail.com>
2022-12-13 01:04:29 -0500

    2022 Day 13

--- src/Year2022/Day13.hs
+++ src/Year2022/Day13.hs
@@ -1,10 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Year2022.Day13
     ( part1
     , part2
     ) where
 
-part1 :: String -> String
-part1 = id
+import Data.List (findIndices, sort)
+import Data.List.Split (splitOn)
+import Data.Maybe
+import Text.Megaparsec
+import Text.Megaparsec.Char.Lexer
 
-part2 :: String -> String
-part2 = const ""
+data Packet = Lit Int | List [Packet] deriving (Eq)
+
+instance Ord Packet where
+    compare (Lit a) (Lit b) = compare a b
+    compare a@(Lit _) b@(List _) = compare (List [a]) b
+    compare a@(List _) b@(Lit _) = compare a $ List [b]
+    compare (List as) (List bs) = foldr (<>) (compare (length as) (length bs)) (zipWith compare as bs)
+
+readPacket :: String -> Packet
+readPacket = fromJust . parseMaybe @() packet
+    where packet = (Lit <$> decimal) <|> (List <$> between "[" "]" (packet `sepBy` ","))
+
+part1 :: String -> Int
+part1 = sum . map check . zip [1..] . splitOn "\n\n"
+    where check (i, pkts) = case map readPacket (lines pkts) of
+                              [p1, p2] -> if p1 < p2 then i else 0
+                              _ -> error "Malformed input"
+
+part2 :: String -> Int
+part2 input = product $ map (+1) $ findIndices (`elem` dividers) packets
+    where dividers = [readPacket "[[2]]", readPacket "[[6]]"]
+          packets = sort $ (dividers ++) $ concatMap (map readPacket . lines) $ splitOn "\n\n" input
