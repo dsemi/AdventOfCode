79bf0243c6d4cc90148e64920b86e8cdce1f7874
Dan Seminara <semi225599@gmail.com>
2022-12-15 16:56:18 -0500

    Small tidy

--- src/Year2022/Day15.hs
+++ src/Year2022/Day15.hs
@@ -4,14 +4,11 @@ module Year2022.Day15
     ) where
 
 import Control.Arrow
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as S
-import Data.List (foldl1')
+import Control.Monad
+import Data.IntSet (IntSet, fromList, toList)
 import Data.List.Split
-import Data.Maybe
-import Linear.V2
 
-data Sensor = Sensor (V2 Int) Int
+data Sensor = Sensor Int Int Int
 
 data Interval = Interval Int Int
 
@@ -22,40 +19,35 @@ len :: Interval -> Int
 len (Interval lo hi) = hi - lo
 
 parse :: String -> ([Sensor], IntSet)
-parse input = (sensors, beaconXs)
-    where (sensors, beacons) = unzip [ (Sensor sPos dist, bPos) | line <- lines input
-                                     , let [_, sx, _, sy, _, bx, _, by] = splitOneOf "=,:" line
-                                     , let (sPos, bPos) = (V2 (read sx) (read sy), V2 (read bx) (read by))
-                                     , let dist = sum $ abs $ sPos - bPos ]
-          beaconXs = S.fromList $ map (\(V2 x _) -> x) $ filter (\(V2 _ y) -> y == 2000000) beacons
+parse input = (sens, beaconXs)
+    where (sens, beacs) = unzip [ (Sensor sx sy dist, (bx, by)) | line <- lines input
+                                , let [sx, sy, bx, by] = map (read . last) $ chunksOf 2 $ splitOneOf "=,:" line
+                                , let dist = abs (sx - bx) + abs (sy - by) ]
+          beaconXs = fromList $ map fst $ filter ((== 2000000) . snd) beacs
 
 part1 :: String -> Int
-part1 input = len interval - length (filter (\b -> b >= lo && b < hi) $ S.toList bs)
+part1 input = len interval - length (filter (\b -> b >= lo && b < hi) $ toList bs)
     where (sensors, bs) = parse input
-          y = 2000000
-          intervals = [ Interval (x - diff) (x + diff + 1) | (Sensor (V2 x sy) dist) <- sensors
-                      , let diff = dist - abs (sy - y)
-                      , diff >= 0 ]
-          interval@(Interval lo hi) = foldl1' union intervals
+          interval@(Interval lo hi) = foldl1 union [ Interval (x - diff) (x + diff + 1)
+                                                   | (Sensor x y dist) <- sensors
+                                                   , let diff = dist - abs (y - 2000000)
+                                                   , diff >= 0 ]
 
-data Line = Line (V2 Int) (V2 Int)
+data Line = Line Int Int Int Int
 
-intersect :: Line -> Line -> Maybe (V2 Int)
-intersect (Line (V2 asx asy) (V2 aex aey)) (Line (V2 bsx bsy) (V2 bex bey))
-    | asx <= bex && bsx <= aex && asy <= bsy && bey <= aey =
-        let (p1, p2) = (bsx + bsy, asx - asy)
-        in Just $ V2 ((p1 + p2) `div` 2) ((p1 - p2) `div` 2)
-    | otherwise = Nothing
+intersect :: Line -> Line -> Maybe (Int, Int)
+intersect (Line asx asy aex aey) (Line bsx bsy bex bey) = do
+  guard $ asx <= bex && bsx <= aex && asy <= bsy && bey <= aey
+  let (p1, p2) = (bsx + bsy, asx - asy)
+  Just ((p1 + p2) `div` 2, (p1 - p2) `div` 2)
 
 part2 :: String -> Int
 part2 input = head [ 4000000*x + y | a <- urs, b <- drs
-                   , pos@(V2 x y) <- maybeToList $ intersect a b
+                   , let (x, y) = maybe (-1, -1) id $ intersect a b
                    , x >= 0 && x <= 4000000 && y >= 0 && y <= 4000000
-                   , all (\(Sensor sp dist) -> sum (abs $ pos - sp) > dist) sensors ]
+                   , all (\(Sensor sx sy dist) -> abs (sx - x) + abs (sy - y) > dist) sensors ]
     where sensors = fst $ parse input
           (urs, drs) = (concat *** concat) $ unzip
-                       [ ( [ Line (V2 (x - dist - 1) y) (V2 x (y + dist + 1))
-                           , Line (V2 x (y - dist - 1)) (V2 (x + dist + 1) y) ]
-                         , [ Line (V2 x (y + dist + 1)) (V2 (x + dist + 1) y)
-                           , Line (V2 (x - dist - 1) y) (V2 x (y - dist - 1)) ])
-                       | (Sensor (V2 x y) dist) <- sensors]
+                       [ ( [ Line (x - dist - 1) y x (y + dist + 1), Line x (y - dist - 1) (x + dist + 1) y ]
+                         , [ Line x (y + dist + 1) (x + dist + 1) y, Line (x - dist - 1) y x (y - dist - 1) ] )
+                       | (Sensor x y dist) <- sensors]
