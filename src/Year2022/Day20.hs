a2af65be2edd4ee1303650e008ca4431ef25fef4
Dan Seminara <semi225599@gmail.com>
2022-12-20 03:48:31 -0500

    2022 Day 20

--- src/Year2022/Day20.hs
+++ src/Year2022/Day20.hs
@@ -3,8 +3,34 @@ module Year2022.Day20
     , part2
     ) where
 
-part1 :: String -> String
-part1 = id
+import Control.Monad
+import Control.Monad.ST
+import Data.List (elemIndex)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
 
-part2 :: String -> String
-part2 = const ""
+mix :: Int -> Int -> String -> Int
+mix scale times input = runST $ do
+  locs <- V.thaw locations
+  forM_ [1..times] $ \_ ->
+      forM_ (zip [0..] ns) $ \(i, n) -> do
+        v <- V.unsafeFreeze locs
+        let Just loc = V.elemIndex i v
+        MV.move (MV.slice loc (m - loc - 1) locs) (MV.slice (loc + 1) (m - loc - 1) locs)
+        let idx = (n + loc) `mod` (m - 1)
+        MV.move (MV.slice (idx + 1) (m - idx - 1) locs) (MV.slice idx (m - idx - 1) locs)
+        MV.write locs idx i
+  v <- V.freeze locs
+  let Just z = elemIndex 0 ns >>= (`V.elemIndex` v)
+  pure $ ns !! (v V.! ((z + 1000) `mod` m)) +
+       ns !! (v V.! ((z + 2000) `mod` m)) +
+       ns !! (v V.! ((z + 3000) `mod` m))
+    where ns = map ((*scale) . read) $ lines input
+          m = length ns
+          locations = V.fromList $ [0 .. m - 1]
+
+part1 :: String -> Int
+part1 = mix 1 1
+
+part2 :: String -> Int
+part2 = mix 811589153 10
