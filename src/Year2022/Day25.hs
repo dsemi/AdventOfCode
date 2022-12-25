baaaaf5c3fb650a6ad7fdfcd03edea3465d7467b
Dan Seminara <semi225599@gmail.com>
2022-12-25 00:31:19 -0500

    2022 Day 25

--- src/Year2022/Day25.hs
+++ src/Year2022/Day25.hs
@@ -3,8 +3,17 @@ module Year2022.Day25
     , part2
     ) where
 
+import Data.List (elemIndex)
+import Data.Maybe
+
+(!) :: (Eq a) => [a] -> a -> Int
+(!) xs x = fromJust $ elemIndex x xs
+
 part1 :: String -> String
-part1 = id
+part1 = reverse . g . sum . map (f . reverse) . lines
+    where f line = sum [ 5^i * ("=-012" ! c - 2) | (i, c) <- zip [0..] line]
+          g 0 = []
+          g n = "012=-" !! (n `mod` 5) : g ((n + 2) `div` 5)
 
 part2 :: String -> String
-part2 = const ""
+part2 = const " "
