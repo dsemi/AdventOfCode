006a38b7f4dd3fc2fb6ee8c65a6acb77a48668ea
Dan Seminara <semi225599@gmail.com>
2023-02-07 18:23:58 -0500

    Set better upper bound

--- src/Year2022/Day19.hs
+++ src/Year2022/Day19.hs
@@ -29,17 +29,24 @@ blueprints = map bp . lines
 sim :: Int32 -> Blueprint -> Int32
 sim time (Blueprint _ costs maxCosts) = dfs 0 time (V.fromList [0, 0, 0, 0]) (V.fromList [0, 0, 0, 1]) 0
     where dfs :: Int32 -> Int32 -> Vector Int32 -> Vector Int32 -> Word8 -> Int32
-          dfs result t amts bots bans
-              | t == 0 = max result $ amts ! 0
+          dfs result tim amts bots bans
+              | tim == 0 = max result geodes
               | upperBd <= result = result
               | otherwise = let (res, bns) = foldl' go (result, bans) $ zip [0..] costs
-                            in dfs res (t-1) (V.zipWith (+) amts bots) bots bns
-              where upperBd = amts ! 0 + t*(bots ! 0) + t*(t+1) `div` 2
+                            in dfs res (tim-1) (V.zipWith (+) amts bots) bots bns
+              where geodes = amts ! 0
+                    geodeBots = bots ! 0
+                    upperBd = upper (geodes + tim * geodeBots) (amts ! 1) (bots ! 1) (tim-1)
+                    obsCost = costs !! 0 ! 1
+                    upper ub obs obsRate t
+                        | t == 0 = ub
+                        | obs >= obsCost = upper (ub + t) (obs + obsRate - obsCost) obsRate (t-1)
+                        | otherwise = upper ub (obs + obsRate) (obsRate + 1) (t-1)
                     go (res, bns) (i, cost)
                         | bns .&. shiftL 1 i == 0 &&
                           (i == 0 || bots ! i < maxCosts ! i) &&
                           V.and (V.zipWith (>=) amts cost) =
-                              ( dfs res (t-1) (V.zipWith3 (\a b c -> a + b - c) amts bots cost)
+                              ( dfs res (tim-1) (V.zipWith3 (\a b c -> a + b - c) amts bots cost)
                                         (V.zipWith (+) bots $ V.replicate 4 0 V.// [(i, 1)]) 0
                               , bns .|. shiftL 1 i )
                         | otherwise = (res, bns)
