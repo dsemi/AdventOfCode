b24372f2db223274ee6bc23a3083abb6eb2ccf22
Dan Seminara <semi225599@gmail.com>
2023-02-22 14:38:56 -0500

    Faster impl

--- src/Year2017/Day24.hs
+++ src/Year2017/Day24.hs
@@ -3,19 +3,15 @@ module Year2017.Day24
     , part2
     ) where
 
-import Control.Arrow
-import Data.List (delete)
+import Control.Monad
+import Control.Monad.ST
+import Data.List (foldl')
 import Data.List.Split
-
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector.Unboxed as VU
 
 type Pipe = (Int, Int)
-type Bridge = (Int, [Pipe])
-
-connect :: Bridge -> Pipe -> Maybe Bridge
-connect (pins, pipes) pipe@(a, b)
-    | pins == a = Just (b, pipe : pipes)
-    | pins == b = Just (a, pipe : pipes)
-    | otherwise = Nothing
 
 parsePipes :: String -> [Pipe]
 parsePipes = map parse . lines
@@ -23,18 +19,25 @@ parsePipes = map parse . lines
           f [x, y] = (read x, read y)
           f _ = error "Invalid pipe"
 
-combos :: [Pipe] -> [[Pipe]]
-combos = go (0, [])
-    where go br [] = [ snd br ]
-          go br ps = [ x | pipe <- ps
-                     , Just br' <- [connect br pipe]
-                     , x <- snd br' : go br' (delete pipe ps) ]
-
-strength :: [Pipe] -> Int
-strength = sum . map (uncurry (+))
+solve :: (Ord a) => (a -> Pipe -> a) -> a -> String -> a
+solve f start input = go 0 start $ VU.replicate (VU.length pipes) False
+    where pipes = VU.fromList $ parsePipes input
+          mx = succ $ VU.maximum $ VU.map (uncurry max) pipes
+          (as, bs) = runST $ do
+            l <- MV.replicate mx VU.empty
+            r <- MV.replicate mx VU.empty
+            forM_ [0 .. VU.length pipes - 1] $ \p -> do
+              let (a, b) = pipes VU.! p
+              MV.modify l (`VU.snoc` p) a
+              when (a /= b) $ MV.modify r (`VU.snoc` p) b
+            (,) <$> V.freeze l <*> V.freeze r
+          go port curr used = foldl' (\cur -> VU.foldl' g cur . (V.! port)) curr [as, bs]
+              where g m p | used VU.! p = m
+                          | otherwise = max m $ go (a + b - port) (f curr pipe) $ used VU.// [(p, True)]
+                          where pipe@(a, b) = pipes VU.! p
 
 part1 :: String -> Int
-part1 = maximum . map strength . combos . parsePipes
+part1 = solve (\s (a, b) -> s + a + b) 0
 
 part2 :: String -> Int
-part2 = snd . maximum . map (length &&& strength) . combos . parsePipes
+part2 = snd . solve (\(l, s) (a, b) -> (l + 1, s + a + b)) (0, 0)
