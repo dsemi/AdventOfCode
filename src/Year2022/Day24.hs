2e7d1e119f18d8db55270ee6611df03fb43bb40e
Dan Seminara <semi225599@gmail.com>
2023-03-02 19:10:49 -0500

    Remove a bunch of unnecessary temp vals

--- src/Year2022/Day24.hs
+++ src/Year2022/Day24.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, RecordWildCards, NamedFieldPuns #-}
+{-# LANGUAGE BangPatterns, RecordWildCards #-}
 
 module Year2022.Day24
     ( part1
@@ -10,21 +10,21 @@ import Control.Monad.ST
 import Control.Monad.State.Strict
 import Data.Bits
 import Data.List (transpose)
-import Data.Vector.Unboxed (Vector)
+import Data.Vector.Unboxed ((!), Vector)
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as MV
 import Data.Word
 
-data Valley = Valley { w :: Int
-                     , h :: Int
-                     , nBlizz :: Vector Word64
-                     , sBlizz :: Vector Word64
-                     , wBlizz :: Vector Word64
-                     , eBlizz :: Vector Word64
-                     , walls :: Vector Word64
-                     }
+data Valley a = Valley { w :: Int
+                       , h :: Int
+                       , nBlizz :: a
+                       , sBlizz :: a
+                       , wBlizz :: a
+                       , eBlizz :: a
+                       , walls :: Vector Word64
+                       } deriving (Functor, Foldable, Traversable)
 
-parse :: String -> Valley
+parse :: String -> Valley (Vector Word64)
 parse input = Valley { w = length grid
                      , h = length (head grid)
                      , nBlizz = readIns '^'
@@ -36,37 +36,34 @@ parse input = Valley { w = length grid
     where grid = transpose $ lines input
           readIns d = V.fromList $ map (foldr (\c acc -> shiftL acc 1 .|. (if c == d then 1 else 0)) 0) grid
 
-shortestPath :: (Int, Int)-> (Int, Int) -> Valley -> (Int, Valley)
-shortestPath (startR, startC) (goalR, goalC) (Valley{..}) = runST $ do
-  frontier <- MV.replicate w (0 :: Word64)
+shortestPath :: (Int, Int)-> (Int, Int) -> Valley (Vector Word64) -> (Int, Valley (Vector Word64))
+shortestPath (startR, startC) (goalR, goalC) valley = runST $ do
+  frontier <- MV.replicate (w valley) (0 :: Word64)
   MV.modify frontier (`setBit` startR) startC
-  (nb, sb, wb, eb) <- (,,,) <$> V.thaw nBlizz <*> V.thaw sBlizz <*> V.thaw wBlizz <*> V.thaw eBlizz
-  t <- go 0 frontier nb sb wb eb
-  (nb', sb', wb', eb') <- (,,,) <$> V.freeze nb <*> V.freeze sb <*> V.freeze wb <*> V.freeze eb
-  pure (t, Valley {w, h, nBlizz = nb', sBlizz = sb', wBlizz = wb', eBlizz = eb', walls})
-    where go !t frontier nBliz sBliz wBliz eBliz = do
+  traverse V.thaw valley >>= go 0 frontier
+    where go !t frontier val@Valley{..} = do
             done <- (`testBit` goalR) <$> MV.read frontier goalC
-            if done then pure t
+            if done then (t,) <$> traverse V.freeze val
             else do
-              pwBliz <- V.freeze wBliz
-              peBliz <- V.freeze eBliz
+              pwBliz <- V.freeze wBlizz
+              peBliz <- V.freeze eBlizz
               pFrontier <- V.freeze frontier
               forM_ [0..w-1] $ \c -> do
-                MV.modify nBliz (\v -> (shiftR v 1 .|. shiftL (v .&. 2) (h - 3)) .&. complement (walls V.! c)) c
-                MV.modify sBliz (\v -> (shiftL v 1 .|. shiftR v (h - 3) .&. 2) .&. complement (walls V.! c)) c
-                MV.write wBliz c (pwBliz V.! ((c - 2 + w) `mod` (w - 2) + 1))
-                MV.write eBliz c (peBliz V.! ((c - 4 + w) `mod` (w - 2) + 1))
-                MV.modify frontier (\v -> v .|. shiftR (pFrontier V.! c) 1 .|.
-                                          shiftL (pFrontier V.! c) 1 .|.
-                                          (pFrontier V.! ((c + 1) `mod` w)) .|.
-                                          (pFrontier V.! ((c - 1) `mod` w))) c
+                MV.modify nBlizz (\v -> (shiftR v 1 .|. shiftL (v .&. 2) (h - 3)) .&. complement (walls ! c)) c
+                MV.modify sBlizz (\v -> (shiftL v 1 .|. shiftR v (h - 3) .&. 2) .&. complement (walls ! c)) c
+                MV.write wBlizz c (pwBliz ! (c `mod` (w - 2) + 1))
+                MV.write eBlizz c (peBliz ! ((c - 2) `mod` (w - 2) + 1))
+                MV.modify frontier (\v -> v .|. shiftR (pFrontier ! c) 1 .|.
+                                          shiftL (pFrontier ! c) 1 .|.
+                                          (pFrontier ! ((c + 1) `mod` w)) .|.
+                                          (pFrontier ! ((c - 1) `mod` w))) c
                 MV.modifyM frontier (\v -> do
-                                       (nb, sb, wb, eb) <- (,,,) <$> MV.read nBliz c <*>
-                                                           MV.read sBliz c <*>
-                                                           MV.read wBliz c <*>
-                                                           MV.read eBliz c
-                                       pure $ v .&. complement ((walls V.! c) .|. nb .|. sb .|. wb .|. eb)) c
-              go (t+1) frontier nBliz sBliz wBliz eBliz
+                                       nb <- MV.read nBlizz c
+                                       sb <- MV.read sBlizz c
+                                       wb <- MV.read wBlizz c
+                                       eb <- MV.read eBlizz c
+                                       pure $ v .&. complement ((walls ! c) .|. nb .|. sb .|. wb .|. eb)) c
+              go (t+1) frontier val
 
 part1 :: String -> Int
 part1 input = fst $ shortestPath (0, 1) (h valley - 1, w valley - 2) valley
