356e566b7c87e35509375e8ea2f5cc95644d9fc3
Dan Seminara <semi225599@gmail.com>
2023-04-02 13:06:45 -0400

    Use flatparse in more places

--- src/Year2020/Day04.hs
+++ src/Year2020/Day04.hs
@@ -1,46 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Year2020.Day04
     ( part1
     , part2
     ) where
 
-
+import Control.Applicative hiding ((<|>))
 import Control.Monad
-import Data.Char
-import Data.List.Split (splitOn)
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Maybe
-import Text.Megaparsec
-import Text.Megaparsec.Char (char, hexDigitChar, lowerChar, space)
-import Text.Megaparsec.Char.Lexer (decimal)
+import Data.ByteString (ByteString)
+import Data.Char (isHexDigit, isLower, isSpace)
+import qualified Data.HashSet as S
+import FlatParse.Basic as F
 
+import Utils
 
-validPassports :: Bool -> String -> Int
-validPassports validateFields = length . mapMaybe (parseMaybe (passport S.empty)) . splitOn "\n\n"
+validPassports :: Bool -> ByteString -> Int
+validPassports validateFields = length . filter parse . splitOn "\n\n"
     where reqFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-          passport :: Set String -> Parsec () String ()
           passport s = try (do
-            field <- some lowerChar <* char ':'
+            field <- byteStringOf (some $ satisfy isLower) <* $(char ':')
             if validateFields then
                 case field of
-                  "byr" -> decimal >>= \n -> guard $ 1920 <= n && n <= 2002
-                  "iyr" -> decimal >>= \n -> guard $ 2010 <= n && n <= 2020
-                  "eyr" -> decimal >>= \n -> guard $ 2020 <= n && n <= 2030
-                  "hgt" -> do h <- decimal
-                              u <- takeP Nothing 2
+                  "byr" -> anyAsciiDecimalInt >>= \n -> guard $ 1920 <= n && n <= 2002
+                  "iyr" -> anyAsciiDecimalInt >>= \n -> guard $ 2010 <= n && n <= 2020
+                  "eyr" -> anyAsciiDecimalInt >>= \n -> guard $ 2020 <= n && n <= 2030
+                  "hgt" -> do h <- anyAsciiDecimalInt
+                              u <- F.take 2
                               guard $ u == "cm" && 150 <= h && h <= 193
                                       || u == "in" && 59 <= h && h <= 76
-                  "hcl" -> void $ char '#' >> count 6 hexDigitChar
-                  "ecl" -> void $ choice $ map chunk [ "amb", "blu", "brn", "gry"
-                                                     , "grn", "hzl", "oth"]
-                  "pid" -> void $ count 9 (satisfy isDigit)
-                  _ -> void $ some $ satisfy $ not . isSpace
-            else void $ some $ satisfy $ not . isSpace
-            space
+                  "hcl" -> $(char '#') >> replicateM_ 6 (satisfy isHexDigit)
+                  "ecl" -> asum $ [ $(string "amb"), $(string "blu"), $(string "brn"), $(string "gry")
+                                  , $(string "grn"), $(string "hzl"), $(string "oth") ]
+                  "pid" -> replicateM_ 9 (satisfy isDigit)
+                  _ -> skipSome $ satisfy $ not . isSpace
+            else skipSome $ satisfy $ not . isSpace
+            skipMany $ satisfy isSpace
             passport (S.insert field s)) <|> guard (all (`S.member` s) reqFields)
+          parse pass = case runParser (passport S.empty) pass of
+                         OK _ _ -> True
+                         _ -> False
 
-part1 :: String -> Int
+part1 :: ByteString -> Int
 part1 = validPassports False
 
-part2 :: String -> Int
+part2 :: ByteString -> Int
 part2 = validPassports True
--- src/Year2020/Day24.hs
+++ src/Year2020/Day24.hs
@@ -1,16 +1,18 @@
-{-# LANGUAGE NegativeLiterals, OverloadedStrings #-}
+{-# LANGUAGE NegativeLiterals #-}
 
 module Year2020.Day24
     ( part1
     , part2
     ) where
 
+import Control.Applicative
 import Data.Bits
-import Data.Maybe
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as M
+import FlatParse.Basic
 import Linear.V3
-import Text.Megaparsec
 
 
 dirs :: [V3 Int -> V3 Int]
@@ -22,12 +24,16 @@ dirs = [ (+ V3  1 -1  0) -- East
        , (+ V3  1  0 -1) -- Northeast
        ]
 
-flipTiles :: String -> HashMap (V3 Int) Bool
+flipTiles :: ByteString -> HashMap (V3 Int) Bool
 flipTiles = M.filter id . M.fromListWith xor . map ((,True) . foldr ($) (V3 0 0 0) . reverse)
-            . map (fromJust . parseMaybe @() pDirs) . lines
-    where pDirs = some $ choice $ zipWith (<$) dirs ["e", "se", "sw", "w", "nw", "ne"]
+            . map parse . B.lines
+    where pDirs = some $ asum $ zipWith (<$) dirs [ $(string "e"), $(string "se"), $(string "sw")
+                                                  , $(string "w"), $(string "nw"), $(string "ne") ]
+          parse line = case runParser pDirs line of
+                         OK res _ -> res
+                         _ -> error "unreachable"
 
-part1 :: String -> Int
+part1 :: ByteString -> Int
 part1 = M.size . flipTiles
 
 step :: HashMap (V3 Int) Bool -> HashMap (V3 Int) Bool
@@ -35,5 +41,5 @@ step m = M.filter id $ M.mapWithKey black adj
     where adj = M.fromListWith (+) $ map (,1) $ dirs <*> M.keys m
           black k v = if M.lookupDefault False k m then v /= 0 && v <= 2 else v == 2
 
-part2 :: String -> Int
+part2 :: ByteString -> Int
 part2 = M.size . (!! 100) . iterate step . flipTiles
