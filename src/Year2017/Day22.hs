19b1f0cf7959738285314a01d6e3102b0bd5dbfb
Dan Seminara <semi225599@gmail.com>
2023-04-08 19:49:35 -0400

    1d vector

--- src/Year2017/Day22.hs
+++ src/Year2017/Day22.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Year2017.Day22
     ( part1
     , part2
@@ -5,61 +7,41 @@ module Year2017.Day22
 
 import Control.Monad
 import Control.Monad.ST
-import Control.Lens
-import Data.Array.ST
-import Data.STRef
+import qualified Data.Vector.Unboxed.Mutable as MV
+import Data.Word
 
+data NodeState = Cleaned | Weakened | Infected | Flagged deriving (Enum)
 
-type Coord = (Int, Int)
-data NodeState = Cleaned | Weakened | Infected | Flagged deriving (Eq, Enum)
-type Grid s = STUArray s Coord Int
-data Direction = U | D | L | R
-
-right :: Direction -> Direction
-right U = R
-right R = D
-right D = L
-right L = U
-
-left :: Direction -> Direction
-left U = L
-left L = D
-left D = R
-left R = U
-
-turn :: NodeState -> Direction -> Direction
-turn Cleaned  = left
-turn Weakened = id
-turn Infected = right
-turn Flagged  = right . right
-
-move :: Direction -> Coord -> Coord
-move U = over _1 pred
-move D = over _1 succ
-move L = over _2 pred
-move R = over _2 succ
+turn :: Word8 -> NodeState -> Word8
+turn d  Cleaned = (d + 3) `rem` 4
+turn d Weakened = d
+turn d Infected = (d + 1) `rem` 4
+turn d  Flagged = (d + 2) `rem` 4
 
 countInfections :: Int -> (NodeState -> NodeState) -> String -> Int
 countInfections bursts nextState input =
     runST $ do
-      count <- newSTRef 0
-      grid <- newArray ((-5000, -5000), (5000, 5000)) $ fromEnum Cleaned
-      forM_ parsedGrid $ \(k, v) -> writeArray grid k $ fromEnum v
-      go count bursts (0, 0) U grid
-    where parsedGrid =
-              [ ((r, c), s) | (r, row) <- zip [-12..] $ lines input
-              , (c, v) <- zip [-12..] row
-              , let s = if v == '#' then Infected else Cleaned ]
-          go :: STRef s Int -> Int -> Coord -> Direction -> Grid s -> ST s Int
-          go count 0 _ _ _ = readSTRef count
-          go count n pos dir grid = do
-            val <- toEnum <$> readArray grid pos
-            let dir' = turn val dir
-                val' = nextState val
-            when (val' == Infected) $
-                 modifySTRef' count (+1)
-            writeArray grid pos $ fromEnum val'
-            go count (n-1) (move dir' pos) dir' grid
+      grid <- MV.replicate (gridSize^2) $ fromEnum Cleaned
+      forM_ (zip [mid-12..mid+12] $ lines input) $ \(r, row) ->
+          forM_ (zip [mid-12..mid+12] row) $ \(c, v) ->
+              when (v == '#') $ MV.write grid (r * gridSize + c) $ fromEnum Infected
+      go grid bursts 0 (mid*gridSize + mid) 3
+    where gridSize = 1000
+          mid = gridSize `div` 2
+          next = fromEnum . nextState . toEnum
+          go _ 0 !count _ _ = pure count
+          go grid n !count pos dir = do
+            dir' <- turn dir . toEnum <$> MV.read grid pos
+            MV.modify grid next pos
+            count' <- (\case Infected -> count + 1
+                             _ -> count) . toEnum <$> MV.read grid pos
+            let pos' = case dir' of
+                         0 -> pos + 1
+                         1 -> pos + gridSize
+                         2 -> pos - 1
+                         3 -> pos - gridSize
+                         _ -> undefined
+            go grid (n-1) count' pos' dir'
 
 part1 :: String -> Int
 part1 = countInfections 10000 f
