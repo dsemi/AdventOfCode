4240b94cf3fc80cadd318e63d3fe0b88006e03bf
Dan Seminara <semi225599@gmail.com>
2023-08-21 16:46:38 -0400

    Remove fromJust in 2022 Day 3

--- src/Year2022/Day03.hs
+++ src/Year2022/Day03.hs
@@ -3,23 +3,21 @@ module Year2022.Day03
     , part2
     ) where
 
-import Data.List (elemIndex)
 import Data.List.Split
-import Data.Maybe
 import qualified Data.HashSet as S
 
-alpha :: String
-alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha :: [(Char, Int)]
+alpha = zip "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" [1..]
 
-part1 :: String -> Int
-part1 = sum . map go . lines
+part1 :: String -> Maybe Int
+part1 = fmap sum . mapM go . lines
     where go line = let (a, b) = splitAt (length line `div` 2) line
                         x = head $ S.toList $ S.intersection (S.fromList a) (S.fromList b)
-                    in fromJust (x `elemIndex` alpha) + 1
+                    in lookup x alpha
 
-part2 :: String -> Int
-part2 = sum . map go . chunksOf 3 . lines
+part2 :: String -> Maybe Int
+part2 = fmap sum . mapM go . chunksOf 3 . lines
     where go [a, b, c] = let x = head $ S.toList
                                  $ foldr1 S.intersection [S.fromList a, S.fromList b, S.fromList c]
-                    in fromJust (x `elemIndex` alpha) + 1
+                         in lookup x alpha
           go _ = error "Malformed input"
